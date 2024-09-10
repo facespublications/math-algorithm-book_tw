58700c5ce1abbbebc35b88efa216fb1f7f042435
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:08:21 +0800

    Update Code_4_05_3.c

--- code/c/Code_4_05_3.c
+++ code/c/Code_4_05_3.c
@@ -1,4 +1,4 @@
-// 隣接リストの作り方はコード 4.5.1 のコメントアウトを参照
+// 鄰接表的做法請參考程式碼 4.5.1 的註解。
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,26 +10,26 @@ int* G[100009];
 int dist[100009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d%d", &N, &M);
 	int i;
 	for (i = 1; i <= M; i++) {
 		scanf("%d%d", &A[i], &B[i]);
 	}
 	
-	// 各頂点の次数を数える（手順 1.）
+	// 計算各頂點的次數（步驟 1.）
 	for (i = 1; i <= N; i++) degree[i] = 0;
 	for (i = 1; i <= M; i++) {
 		degree[A[i]] += 1;
 		degree[B[i]] += 1;
 	}
 	
-	// 隣接リスト G の構築（手順 2.）
+	// 建構鄰接表 G （步驟 2.）
 	for (i = 1; i <= N; i++) {
 		G[i] = (int*)malloc(degree[i] * sizeof(int));
 	}
 	
-	// G に辺の情報を追加していく（手順 3.）
+	// 向 G 追加邊的資訊（步驟 3.）
 	for (i = 1; i <= N; i++) cnt[i] = 0;
 	for (i = 1; i <= M; i++) {
 		G[A[i]][cnt[A[i]]] = B[i];
@@ -39,38 +39,38 @@ int main() {
 	}
 	
 	
-	// キュー Q の定義
-	// キューの中身が Q[QL], Q[QL+1], ..., Q[QR-1] になるようにする
-	// （詳しい原理は https://qiita.com/square1001/items/6d414167ca95c97bd8b2 の 5 節を参照）
+	// 定義佇列 Q
+	// 讓佇列 Q 的內容變成 Q[QL], Q[QL+1], ..., Q[QR-1] 
+	// （詳細的原理請參照 https://qiita.com/square1001/items/6d414167ca95c97bd8b2 的第 5 節）
 	int Q[100009];
 	int QL = 0, QR = 0;
 	
-	// 幅優先探索の初期化（dist[i] = -1 のとき、未到達の白色頂点である）
+	// 廣度優先搜尋的初始化（dist[i]=-1 時，為未到達的白色頂點）
 	for (i = 1; i <= N; i++) {
 		dist[i] = -1;
 	}
 	dist[1] = 0;
-	Q[QR] = 1; QR++; // Q に 1 を追加（操作 1）
+	Q[QR] = 1; QR++; // 將 1 添加到 Q 中（操作 1）
 	
-	// 幅優先探索
+	// 廣度優先搜尋
 	while (QL != QR) {
-		int pos = Q[QL]; // Q の先頭を調べる（操作 2）
-		QL++; // Q の先頭を取り出す（操作 3）
+		int pos = Q[QL]; // 查看 Q 的開頭（操作2）
+		QL++; // 取出Q 的開頭（操作3）
 		for (i = 0; i < degree[pos]; i++) {
 			int nex = G[pos][i];
 			if (dist[nex] == -1) {
 				dist[nex] = dist[pos] + 1;
-				Q[QR] = nex; QR++; // Q に nex を追加（操作 1）
+				Q[QR] = nex; QR++; // 將nex 添加到Q 中（操作1）
 			}
 		}
 	}
 	
-	// 頂点 1 から各頂点までの最短距離を出力
+	// 輸出從頂點1 到各頂點的最短距離
 	for (i = 1; i <= N; i++) {
 		printf("%d\n", dist[i]);
 	}
 	
-	// 隣接リスト G の削除
+	// 刪除鄰接表 G 
 	for (i = 1; i <= N; i++) {
 		free(G[i]);
 	}
