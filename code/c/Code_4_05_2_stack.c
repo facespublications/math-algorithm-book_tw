30d1bb7eb91d4927b509bf2241b39eda1294acb7
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:05:14 +0800

    Update Code_4_05_2_stack.c

--- code/c/Code_4_05_2_stack.c
+++ code/c/Code_4_05_2_stack.c
@@ -1,8 +1,7 @@
-// このソースコードは、深さ優先探索 (DFS) をスタックを用いて実装したものです。
-// スタックは「一番上に要素を積む」「一番上の要素を調べる」「一番上に積まれた要素を取り除く」という 3 種類の操作ができるデータ構造です。
-// 深さ優先探索の部分は、コード 4.5.3 の queue を stack に変更したものをベースに書かれています。
-
-// 隣接リストの作り方はコード 4.5.1 のコメントを参照
+// 這個原始碼為將深度優先搜尋 (DFS) 使用了堆疊的實作例。
+// 堆疊為可進行「在最上層堆積元素」、「查找堆積在最上層的元素」、「刪除堆積在最上層的元素」三種操作的資料結構。
+// 深度優先搜尋的部分為將程式碼 4.5.3 的 queue 變更為 stack，以此為基礎撰寫。
+// 鄰接表的做法請參考程式碼 4.5.1 的註解。
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,26 +13,26 @@ int* G[100009];
 bool visited[100009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d%d", &N, &M);
 	int i;
 	for (i = 1; i <= M; i++) {
 		scanf("%d%d", &A[i], &B[i]);
 	}
 	
-	// 各頂点の次数を数える（手順 1.）
+	// 計算各頂點的次數（步驟 1.）
 	for (i = 1; i <= N; i++) degree[i] = 0;
 	for (i = 1; i <= M; i++) {
 		degree[A[i]] += 1;
 		degree[B[i]] += 1;
 	}
 	
-	// 隣接リスト G の構築（手順 2.）
+	// 建構鄰接表 G （步驟 2.）
 	for (i = 1; i <= N; i++) {
 		G[i] = (int*)malloc(degree[i] * sizeof(int));
 	}
 	
-	// G に辺の情報を追加していく（手順 3.）
+	// 向 G 追加邊的資訊（步驟 3.）
 	for (i = 1; i <= N; i++) cnt[i] = 0;
 	for (i = 1; i <= M; i++) {
 		G[A[i]][cnt[A[i]]] = B[i];
@@ -43,31 +42,31 @@ int main() {
 	}
 	
 	
-	// スタック S の定義
-	// スタックの中身が S[0], S[1], ..., S[SZ - 1] になるようにする
+	// 定義堆疊 S 
+	// 讓堆疊的內容變成 S[0], S[1], ..., S[SZ - 1] 
 	int S[100009], SZ = 0;
 	
-	// 深さ優先探索の初期化
+	// 深度優先搜尋的初始化
 	for (i = 1; i <= N; i++) {
 		visited[i] = false;
 	}
 	visited[1] = true;
-	S[SZ] = 1; SZ++; // S に 1 を追加
+	S[SZ] = 1; SZ++; // S 追加 1
 	
-	// 幅優先探索
+	// 廣度優先搜尋
 	while (SZ >= 1) {
-		int pos = S[SZ - 1]; // S の先頭を調べる
-		SZ--; // S の先頭を取り出す
+		int pos = S[SZ - 1]; // 調查 S 的開頭
+		SZ--; // 取出 S 的開頭
 		for (i = 0; i < degree[pos]; i++) {
 			int nex = G[pos][i];
 			if (visited[nex] == false) {
 				visited[nex] = true;
-				S[SZ] = nex; SZ++; // S に nex を追加
+				S[SZ] = nex; SZ++; // 追加 nex 至 S
 			}
 		}
 	}
 	
-	// 連結かどうかの判定（Answer=true のとき連結）
+	// 判定是否連通（Answer=true 時為連通）
 	bool answer = true;
 	for (int i = 1; i <= N; i++) {
 		if (visited[i] == false) {
@@ -81,7 +80,7 @@ int main() {
 		printf("The graph is not connected.\n");
 	}
 	
-	// 隣接リスト G の削除
+	// 刪除鄰接表 G
 	for (i = 1; i <= N; i++) {
 		free(G[i]);
 	}
