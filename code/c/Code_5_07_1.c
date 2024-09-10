5591c61b04199b5938bcc5511a12f33ee8a040ec
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:28:19 +0800

    Update Code_5_07_1.c

--- code/c/Code_5_07_1.c
+++ code/c/Code_5_07_1.c
@@ -3,14 +3,14 @@
 int N; long long A[200009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d", &N);
 	int i;
 	for (i = 1; i <= N; i++) {
 		scanf("%lld", &A[i]);
 	}
 
-	// 答えを求める → 答えの出力
+	// 求解 → 輸出答案
 	long long answer = 0;
 	for (i = 1; i <= N; i++) {
 		answer += A[i] * (-N + 2LL * i - 1LL);
