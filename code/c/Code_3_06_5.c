9acac1797a4e0702ebc2731b31e6f22161baec9e
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:04:45 +0800

    Update Code_3_06_5.c

--- code/c/Code_3_06_5.c
+++ code/c/Code_3_06_5.c
@@ -4,21 +4,21 @@ int N, A[109];
 
 int solve(int l, int r) {
 	if (r - l == 1) return A[l];
-	int m = (l + r) / 2; // 区間 [l, r) の中央で分割する
-	int s1 = solve(l, m); // s1 は A[l]+...+A[m-1] の合計値となる
-	int s2 = solve(m, r); // s2 は A[m]+...+A[r-1] の合計値となる
+	int m = (l + r) / 2; // 在區間 [l,r)的中央進行分割
+	int s1 = solve(l, m); // s1為 A[l]+...+A[m-1]的合計值
+	int s2 = solve(m, r); // s2為 A[m]+...+A[r-1] 的合計值
 	return s1 + s2;
 }
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d", &N);
 	int i;
 	for (i = 1; i <= N; i++) {
 		scanf("%d", &A[i]);
 	}
 
-	// 再帰呼び出し → 答えの出力
+	// 遞迴呼叫→輸出答案
 	int answer = solve(1, N + 1);
 	printf("%d\n", answer);
 	return 0;
