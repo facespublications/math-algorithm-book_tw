f6c5cadc9638b7c70209498e85f0a718409d6347
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:48:53 +0800

    Update Code_5_09_2.cpp

--- code/cpp/Code_5_09_2.cpp
+++ code/cpp/Code_5_09_2.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int N, L[2009], R[2009];
-int Current_Time = 0, Answer = 0; // Current_Time �͌��ݎ����i���O�Ɍ����f��̏I�������j
+int Current_Time = 0, Answer = 0; // Current_Time為現在時間（之前看完的電影的結束時間）
 
 int main() {
-	// ����
+	// 輸入
 	cin >> N;
 	for (int i = 1; i <= N; i++) cin >> L[i] >> R[i];
 
-	// �f��̑I�ѕ��̃V�~�����[�V����
-	// �����f��̏I�������̍ŏ��l min_endtime �͍ŏ� 1000000 �̂悤�Ȃ��蓾�Ȃ��l�ɐݒ肷��
+	// 映模擬電影的選擇方法
+	// 可觀看電影的結束時間最小值min_endtime 最初是設定成像1000000 這樣不可能的值
 	while (true) {
 		int min_endtime = 1000000;
 		for (int i = 1; i <= N; i++) {
@@ -23,7 +23,7 @@ int main() {
 		Answer += 1;
 	}
 
-	// �����̏o��
+	// 輸出答案
 	cout << Answer << endl;
 	return 0;
 }
