9457b8fd1887eb478fdc7f85be655f0564f7a019
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:02:38 +0800

    Update Code_3_06_1.c

--- code/c/Code_3_06_1.c
+++ code/c/Code_3_06_1.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int compare_values(const void* a, const void* b) {
-	// a の方が前に来るとき 負の値
-	// b の方が前に来るとき 正の値
-	// a と b が等しい値のとき 0 を返す関数
+	// a 先出現時為負值
+	// b 先出現時為正值
+	// a 與 b 為相同值時返回 0 的函數
 	if (*(int*)a < *(int*)b) return -1;
 	if (*(int*)a > *(int*)b) return +1;
 	return 0;
@@ -13,18 +13,18 @@ int compare_values(const void* a, const void* b) {
 int N, A[200009];
 
 int main() {
-	// 入力（たとえば N=5, A[1]=3, A[2]=1, A[3]=4, A[4]=1, A[5]=5 を入力したとする）
+	// 輸入（例如輸入了N=5、A[1]=3、A[2]=1、A[3]=4、A[4]=1,A[5]=5）
 	scanf("%d", &N);
 	int i;
 	for (i = 1; i <= N; i++) {
 		scanf("%d", &A[i]);
 	}
 	
-	// ソート（A[1] から始まる N 個の要素をソートするので、引数に A+1, N を指定している）
-	// qsort 関数により、配列の中身が [3,1,4,1,5] から [1,1,3,4,5] に書き換えられる
+	// 排序（因為要排序從　A[1] 開始的N個元素，因此引數指定　A+1, N）
+	// 藉由 qsort 函式，將陣列的內容[3,1,4,1,5] 改寫為1,1,3,4,5]
 	qsort(A + 1, N, sizeof(int), compare_values);
 	
-	// 出力（1, 1, 3, 4, 5 の順に出力される）
+	// 輸出（按照1、1、3、4、5 的順序輸出）
 	for (i = 1; i <= N; i++) {
 		printf("%d\n", A[i]);
 	}
