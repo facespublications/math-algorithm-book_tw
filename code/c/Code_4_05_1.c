053378fef5f8ce39b693489608fd79842b2bc837
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:21:54 +0800

    Update Code_4_05_1.c

--- code/c/Code_4_05_1.c
+++ code/c/Code_4_05_1.c
@@ -1,7 +1,7 @@
-// C 言語ではコード 4.5.1 の vector<int> に相当するものが使えないので、本コードでは以下の 3 ステップでグラフの隣接リストを構築しています。
-// 手順 1. 各頂点の次数 degree[i] を数える
-// 手順 2. 各 G[i] のメモリを malloc で動的確保する
-// 手順 3. 確保された G[i] に辺の行き先を割り当てていく
+// 在 C 語言中由於無法使用如程式碼 4.5.1 的 vector<int>，本程式碼以下面三個步驟建構圖的鄰接表。
+// 步驟 1. 計算各頂點的次數 degree[i] 
+// 步驟 2. 使用malloc動態確保各 G[i] 的記憶體
+// 步驟 3. 分配被確保的 G[i] 的邊的方向
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,26 +11,26 @@ int degree[100009], cnt[100009];
 int* G[100009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d%d", &N, &M);
 	int i, j;
 	for (i = 1; i <= M; i++) {
 		scanf("%d%d", &A[i], &B[i]);
 	}
 	
-	// 各頂点の次数を数える（手順 1.）
+	// 計算各頂點的次數（步驟 1.）
 	for (i = 1; i <= N; i++) degree[i] = 0;
 	for (i = 1; i <= M; i++) {
 		degree[A[i]] += 1;
 		degree[B[i]] += 1;
 	}
 	
-	// 隣接リスト G の構築（手順 2.）
+	// 建構鄰接表 G （步驟 2.）
 	for (i = 1; i <= N; i++) {
 		G[i] = (int*)malloc(degree[i] * sizeof(int));
 	}
 	
-	// G に辺の情報を追加していく（手順 3.）
+	// 向　G 追加邊的資訊（步驟 3.）
 	for (i = 1; i <= N; i++) cnt[i] = 0;
 	for (i = 1; i <= M; i++) {
 		G[A[i]][cnt[A[i]]] = B[i];
@@ -39,7 +39,7 @@ int main() {
 		cnt[B[i]] += 1;
 	}
 	
-	// 出力
+	// 輸出
 	for (i = 1; i <= N; i++) {
 		printf("%d: {", i);
 		for (j = 0; j < degree[i]; j++) {
@@ -49,7 +49,7 @@ int main() {
 		printf("}\n");
 	}
 	
-	// 隣接リスト G の削除
+	// 刪除鄰接表 G
 	for (i = 1; i <= N; i++) {
 		free(G[i]);
 	}
