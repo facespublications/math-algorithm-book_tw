c67cd5b347cc66557db18e9e3df67eedae7d281e
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 13:55:50 +0800

    Update Code_3_02_2.c

--- code/c/Code_3_02_2.c
+++ code/c/Code_3_02_2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// GCD は Greatest Common Divisor（最大公約数）の略
+// GCD 是Greatest Common Divisor（最大公因數）的縮寫
 long long GCD(long long A, long long B) {
 	while (A >= 1 && B >= 1) {
-		if (A < B) B = B % A; // A < B の場合、大きい方 B を書き換える
-		else A = A % B; // A >= B の場合、大きい方 A を書き換える
+		if (A < B) B = B % A; // A<B時，改寫較大的數B
+		else A = A % B; // A >= B時，改寫較大的數A
 	}
 	if (A >= 1) return A;
 	return B;
