98096b35f3b35ab9ba1bbbaac0fde99adc1aa298
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:06:44 +0800

    Update Code_5_10_3.c

--- code/c/Code_5_10_3.c
+++ code/c/Code_5_10_3.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	// 入力
+	// 輸入
 	int N; long long X, Y;
 	scanf("%d%lld%lld", &N, &X, &Y);
 
-	// 4 つの整数 (a, b, c, d) の全探索 → 答えの出力
+	// 4 個整數(a, b, c, d) 的全搜尋→輸出答案
 	int a, b, c, d;
 	for (a = 1; a <= N; a++) {
 		for (b = a; b <= N; b++) {
@@ -13,12 +13,12 @@ int main() {
 				for (d = c; d <= N; d++) {
 					if (a + b + c + d == X && 1LL * a * b * c * d == Y) {
 						printf("Yes\n");
-						return 0; // プログラムの実行を終了させる
+						return 0; // 使程式結束執行
 					}
 				}
 			}
 		}
 	}
-	printf("No\n"); // 1 つも見つからなかったら No
+	printf("No\n"); // 如果連 1 組都沒發現的話為No
 	return 0;
 }
