680868eacd50fc440c104eecb1dcb25742271e61
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:32:08 +0800

    Update Code_4_02_1.cpp

--- code/cpp/Code_4_02_1.cpp
+++ code/cpp/Code_4_02_1.cpp
@@ -5,14 +5,14 @@ int N, A[100009], B[100009];
 int Q, L[100009], R[100009];
 
 int main() {
-	// “ü—Í ¨ —ÝÏ˜a‚ð‹‚ß‚é
+	// è¼¸å…¥ â†’ æ±‚å‡ºç´¯ç©å’Œ
 	cin >> N >> Q;
 	for (int i = 1; i <= N; i++) cin >> A[i];
 	for (int j = 1; j <= Q; j++) cin >> L[j] >> R[j];
 	B[0] = 0;
 	for (int i = 1; i <= N; i++) B[i] = B[i - 1] + A[i];
 
-	// “š‚¦‚Ìo—Í
+	// è¼¸å‡ºç­”æ¡ˆ
 	for (int j = 1; j <= Q; j++) cout << B[R[j]] - B[L[j] - 1] << endl;
 	return 0;
 }
