37a99c92e840ff8e062c458267cb4970322ad334
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:10:29 +0800

    Update Code_3_07_1.c

--- code/c/Code_3_07_1.c
+++ code/c/Code_3_07_1.c
@@ -4,21 +4,21 @@
 int N, H[100009], dp[100009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d", &N);
 	int i;
 	for (i = 1; i <= N; i++) {
 		scanf("%d", &H[i]);
 	}
 
-	// 動的計画法 → 答えの出力
+	// 動態規劃法 → 輸出答案
 	for (i = 1; i <= N; i++) {
 		if (i == 1) dp[i] = 0;
 		if (i == 2) dp[i] = abs(H[i - 1] - H[i]);
 		if (i >= 3) {
-			int v1 = dp[i - 1] + abs(H[i - 1] - H[i]); // 1 個前の足場からジャンプするとき
-			int v2 = dp[i - 2] + abs(H[i - 2] - H[i]); // 2 個前の足場からジャンプするとき
-			dp[i] = (v1 < v2 ? v1 : v2); // v1 と v2 のうち小さい方の値
+			int v1 = dp[i - 1] + abs(H[i - 1] - H[i]); // 從前　1 個棚架起跳時
+			int v2 = dp[i - 2] + abs(H[i - 2] - H[i]); // 從前　2 個棚架起跳時
+			dp[i] = (v1 < v2 ? v1 : v2); // v1 與 v2 之間較小的那個值
 		}
 	}
 	printf("%d\n", dp[N]);
