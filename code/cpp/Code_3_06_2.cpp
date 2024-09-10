71379800361ae8e5143693a5892491bce1c11d05
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:27:18 +0800

    Update Code_3_06_2.cpp

--- code/cpp/Code_3_06_2.cpp
+++ code/cpp/Code_3_06_2.cpp
@@ -4,23 +4,23 @@ using namespace std;
 int N, A[200009];
 
 int main() {
-	// 入力
+	// 輸入
 	cin >> N;
 	for (int i = 1; i <= N; i++) cin >> A[i];
 
-	// 選択ソート
+	// 選擇排序
 	for (int i = 1; i <= N - 1; i++) {
 		int Min = i, Min_Value = A[i];
 		for (int j = i + 1; j <= N; j++) {
 			if (A[j] < Min_Value) {
-				Min = j; // Min は最小値のインデックス（1～N）
-				Min_Value = A[j]; // Min_Value は現時点での最小値
+				Min = j; // Min 為最小值的索引（1 ∼ N）
+				Min_Value = A[j]; // Min_Value 為當下的最小值
 			}
 		}
 		swap(A[i], A[Min]);
 	}
 
-	// 出力
+	// 輸出
 	for (int i = 1; i <= N; i++) cout << A[i] << endl;
 	return 0;
 }
