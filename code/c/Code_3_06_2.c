85c78991a5ea6eb95f7b6c8ac67fe2248374f07b
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:03:44 +0800

    Update Code_3_06_2.c

--- code/c/Code_3_06_2.c
+++ code/c/Code_3_06_2.c
@@ -3,30 +3,30 @@
 int N, A[200009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d", &N);
 	int i, j;
 	for (i = 1; i <= N; i++) {
 		scanf("%d", &A[i]);
 	}
 
-	// 選択ソート
+	// 選擇排序
 	for (i = 1; i <= N - 1; i++) {
 		int min_position = i;
 		int min_value = A[i];
 		for (j = i + 1; j <= N; j++) {
 			if (A[j] < min_value) {
-				min_position = j; // min_position は最小値のインデックス（1～N）
-				min_value = A[j]; // min_value は現時点での最小値
+				min_position = j; // min_position 為最小值的索引（1 ∼ N）
+				min_value = A[j]; // min_value 為當下的最小值
 			}
 		}
-		// 次の 3 行で A[i] と A[min_position] を交換
+		// 透過下面３行交換 A[i] 與 A[min_position]
 		int temp = A[i];
 		A[i] = A[min_position];
 		A[min_position] = temp;
 	}
 
-	// 出力
+	// 輸出
 	for (int i = 1; i <= N; i++) {
 		printf("%d\n", A[i]);
 	}
