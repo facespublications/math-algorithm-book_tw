b2883744ab0f0dd8708cc72f9ceeda30e6586cf4
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:14:35 +0800

    Update Code_4_01_1.c

--- code/c/Code_4_01_1.c
+++ code/c/Code_4_01_1.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 
 int main() {
-	// 入力
+	// 輸入
 	long long ax, ay, bx, by, cx, cy;
 	scanf("%lld%lld%lld%lld%lld%lld", &ax, &ay, &bx, &by, &cx, &cy);
 
-	// ベクトル BA, BC, CA, CB の成分表示を求める
+	// 求向量BA、BC、CA、CB 的分量表示
 	long long BAx = (ax - bx), BAy = (ay - by);
 	long long BCx = (cx - bx), BCy = (cy - by);
 	long long CAx = (ax - cx), CAy = (ay - cy);
 	long long CBx = (bx - cx), CBy = (by - cy);
 
-	// どのパターンに当てはまるかを判定する
+	// 判斷適用哪一個模式
 	int pattern = 2;
 	if (BAx * BCx + BAy * BCy < 0LL) pattern = 1;
 	if (CAx * CBx + CAy * CBy < 0LL) pattern = 3;
 
-	// 点と直線の距離を求める
+	// 求出點與直線的距離
 	double answer = 0.0;
 	if (pattern == 1) answer = sqrt(BAx * BAx + BAy * BAy);
 	if (pattern == 3) answer = sqrt(CAx * CAx + CAy * CAy);
@@ -28,7 +28,7 @@ int main() {
 		answer = S / BCLength;
 	}
 
-	// 答えの出力
+	// 輸出答案
 	printf("%.12lf\n", answer);
 	return 0;
 }
