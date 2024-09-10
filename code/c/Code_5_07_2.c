0986a6d6f3715ebe7b2f16ac21c7852630b22be7
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:30:09 +0800

    Update Code_5_07_2.c

--- code/c/Code_5_07_2.c
+++ code/c/Code_5_07_2.c
@@ -5,7 +5,7 @@ int N; long long A[200009];
 long long fact[200009];
 
 long long modpow(long long a, long long b, long long m) {
-	// 繰り返し二乗法（p は a^1, a^2, a^4, a^8, ... といった値をとる）
+	// 重複平方法（p 取 a^1、a^2、 a^4、a^8、⋯的值）
 	long long p = a, Answer = 1;
 	int i;
 	for (i = 0; i < 30; i++) {
@@ -20,32 +20,32 @@ long long Division(long long a, long long b, long long m) {
 }
 
 long long ncr(int n, int r) {
-	// ncr は n! を r! × (n-r)! で割った値
+	// ncr 是n! 除以r!×(n-r)! 的值
 	return Division(fact[n], fact[r] * fact[n - r] % mod, mod);
 }
 
 int main() {
-	// 配列の初期化（fact[i] は i の階乗を 1000000007 で割った余り）
+	// 陣列初始化（fact[i] 是i 的階乘除以10000007 的餘數）
 	fact[0] = 1;
 	int i;
 	for (i = 1; i <= 200000; i++) {
 		fact[i] = 1LL * i * fact[i - 1] % mod;
 	}
 
-	// 入力
+	// 輸入
 	scanf("%d", &N);
 	for (i = 1; i <= N; i++) {
 		scanf("%lld", &A[i]);
 	}
 
-	// 答えを求める
+	// 求出答案
 	long long answer = 0;
 	for (i = 1; i <= N; i++) {
 		answer += A[i] * ncr(N - 1, i - 1);
 		answer %= mod;
 	}
 	
-	// 答えの出力
+	// 輸出答案
 	printf("%lld\n", answer);
 	return 0;
 }
