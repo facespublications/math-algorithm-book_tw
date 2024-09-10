ec03456ecca0f301e126ea6377eea99ffe2128f3
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:25:44 +0800

    Update Code_5_04_1.c

--- code/c/Code_5_04_1.c
+++ code/c/Code_5_04_1.c
@@ -10,16 +10,16 @@ int maximum(int a, int b) {
 }
 
 int main() {
-	// 入力
+	// 輸入
 	int N, K;
 	scanf("%d%d", &N, &K);
 
-	// 事象 B の個数 yojishou を数える
+	// 算出事件 B 的個數 yojishou → 輸出答案
 	long long yojishou = 0;
 	int a, b, c;
 	for (a = 1; a <= N; a++) {
-		int l = maximum(a - (K - 1), 1); // b, c の探索範囲の下限 l
-		int r = minimum(a + (K - 1), N); // b, c の探索範囲の上限 r
+		int l = maximum(a - (K - 1), 1); // b, c 的探索範圍下限 l
+		int r = minimum(a + (K - 1), N); // b, c 的探索範圍上限 r
 		for (b = l; b <= r; b++) {
 			for (c = l; c <= r; c++) {
 				if (abs(b - c) <= K - 1) yojishou += 1;
@@ -27,7 +27,7 @@ int main() {
 		}
 	}
 	
-	// 答えの出力
+	// 輸出答案
 	printf("%lld\n", (long long)N * N * N - yojishou);
 	return 0;
 }
