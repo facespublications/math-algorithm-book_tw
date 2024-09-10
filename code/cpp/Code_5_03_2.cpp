910b1b693a79b33ea821ddfc8c8156e2cf0d0672
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:44:28 +0800

    Update Code_5_03_2.cpp

--- code/cpp/Code_5_03_2.cpp
+++ code/cpp/Code_5_03_2.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int N, K, A[59];
-int sum = 0; // A[1] + A[2] + ... + A[N] ‚Ì’l
+int sum = 0; // A[1] + A[2] + ... + A[N] ã®å€¤
 
 int main() {
-	// “ü—Í ¨ ”—ñ‚Ì—v‘f‚Ì‘˜a sum ‚ð‹‚ß‚é
+	// è¼¸å…¥ â†’ æ±‚æ•¸åˆ—å…ƒç´ çš„ç¸½å’Œsum
 	cin >> N >> K;
 	for (int i = 1; i <= N; i++) cin >> A[i];
 	for (int i = 1; i <= N; i++) sum += A[i];
 
-	// “š‚¦‚Ìo—Í
+	// è¼¸å‡ºç­”æ¡ˆ
 	if (sum % 2 != K % 2) cout << "No" << endl;
 	else if (sum > K) cout << "No" << endl;
 	else cout << "Yes" << endl;
