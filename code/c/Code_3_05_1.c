5e00f14a98a37e35cdcdf636773783234a28f130
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 13:59:06 +0800

    Update Code_3_05_1.c

--- code/c/Code_3_05_1.c
+++ code/c/Code_3_05_1.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-	int N = 10000; // N は試行回数（適宜変更する）
+	int N = 10000; // N為測試次數（可適當變更）
 	int M = 0;
 	int i;
 	for (i = 1; i <= N; i++) {
-		double px = rand() / (double)RAND_MAX; // 0 以上 1 以下の乱数（ランダムな数）を生成
-		double py = rand() / (double)RAND_MAX; // 0 以上 1 以下の乱数（ランダムな数）を生成
-		// 原点からの距離は sqrt(px * px + py * py)
-		// これが 1 以下であれば良いので、条件は「px * px + py * py <= 1」
+		double px = rand() / (double)RAND_MAX; // 生成 0 以上1 以下的亂數（隨機的數）
+		double py = rand() / (double)RAND_MAX; // 生成 0 以上1 以下的亂數（隨機的數）
+		// 與原點的距離為sqrt(px * px + py * py)
+		// 此值最好在1 以下，因此條件為「px * px + py * py <= 1」
 		if (px * px + py * py <= 1.0) M += 1;
 	}
 	printf("%.12lf\n", 4.0 * M / N);
