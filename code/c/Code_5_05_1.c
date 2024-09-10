406b45bd84d44a2f32f180adbb5e5010ea06c201
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:26:35 +0800

    Update Code_5_05_1.c

--- code/c/Code_5_05_1.c
+++ code/c/Code_5_05_1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	// 入力
+	// 輸入
 	long long a, b, c, d;
 	scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
 	
-	// 答えを求める（answer は最初あり得ない値に設定）
+	// 求解（answer 一開始設定為不可能的數值）
 	long long answer = -(1LL << 62);
 	if (answer < a * c) answer = a * c;
 	if (answer < a * d) answer = a * d;
 	if (answer < b * c) answer = b * c;
 	if (answer < b * d) answer = b * d;
 	
-	// 出力
+	// 輸出
 	printf("%lld\n", answer);
 	return 0;
 }
