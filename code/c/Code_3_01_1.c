9589d9e88ef614d2df6e711c21c5169bae97b0db
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 12:43:23 +0800

    Update Code_3_01_1.c

--- code/c/Code_3_01_1.c
+++ code/c/Code_3_01_1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// 2 以上の整数 N に対し、N が素数であれば true、素数でなければ false を返す関数
+// 設 N 為2 以上整數，若N 為質數則返回true，若不是質數則返回false
 bool isprime(long long N) {
 	long long i;
 	for (i = 2; i <= N - 1; i++) {
 		if (N % i == 0) {
-			// N が i で割り切れた場合、この時点で素数ではないと分かる
+			// N 被 i 整除時，此時可知不是質數
 			return false;
 		}
 	}
