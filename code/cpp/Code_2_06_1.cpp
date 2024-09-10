290b0167f8cdb47b4ec7f25c04ef984b4d298a1b
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:15:50 +0800

    Update Code_2_06_1.cpp

--- code/cpp/Code_2_06_1.cpp
+++ code/cpp/Code_2_06_1.cpp
@@ -7,12 +7,12 @@ int main() {
 	cin >> N >> S;
 	for (int i = 1; i <= N; i++) cin >> A[i];
 
-	// 全パターンを探索：(1LL << N) は 2 の N 乗
+	// 搜尋全部模式：（1LL << N）為 2 的 N 次方
 	for (long long i = 0; i < (1LL << N); i++) {
 		long long sum = 0;
 		for (int j = 1; j <= N; j++) {
-			// (i & (1LL << (j-1))) != 0LL の場合、i の 2 進法表記の下から j 桁目が 1
-			// (1LL << (j-1)) は C++ では「2 の j-1 乗」を意味します
+			// (i & (1LL << (j-1))) != 0LL 時，i 的二進制表示的倒數第 j 位是1
+			// (1LL << (j-1)) 在 C++ 中代表「2 的 j-1 次方」
 			if ((i & (1LL << (j-1))) != 0LL) sum += A[j];
 		}
 		if (sum == S) { cout << "Yes" << endl; return 0; }
