33e98941fc0075d03deb6e533947f984f0c232ed
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 12:35:22 +0800

    Update Code_2_01_4.c

--- code/c/Code_2_01_4.c
+++ code/c/Code_2_01_4.c
@@ -4,17 +4,17 @@
 int main() {
 	int N;
 	scanf("%d", &N);
-	char answer[100] = ""; // char 型の配列で文字列を表す
+	char answer[100] = ""; // 以char型的陣列表示字串
 	while (N >= 1) {
-		// N % 2 は N を 2 で割った余り（例：N = 13 の場合 1）
-		// N / 2 は N を 2 で割った値の整数部分（例：N = 13 の場合 6）
+		// N % 2 為 N 除以 2 的餘數（例如：N=13 時為1）
+		// N / 2 為N 除以2 的值的整數部分（例如：N=13 時為6）
 		char next_answer[101];
 		if (N % 2 == 0) {
-			// next_answer に「"0" と answer をつなげたもの」を代入
+			// next_answer 代入「連結 "0" 和 answer 的東西」
 			sprintf(next_answer, "0%s", answer);
 		}
 		if (N % 2 == 1) {
-			// next_answer に「"1" と answer をつなげたもの」を代入
+			// next_answer 代入「連結 "1" 和 answer 的東西」
 			sprintf(next_answer, "1%s", answer);
 		}
 		strcpy(answer, next_answer);
