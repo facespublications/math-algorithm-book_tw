0c640b9e1d5a53b596d3987714302f55280754d7
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 18:20:00 +0800

    Update Code_3_02_1.cpp

--- code/cpp/Code_3_02_1.cpp
+++ code/cpp/Code_3_02_1.cpp
@@ -1,5 +1,5 @@
-// ���̐��� A �� B �̍ő���񐔂�Ԃ��֐�
-// GCD �� Greatest Common Divisor�i�ő���񐔁j�̗�
+// 返回正整數 A 和 B 的最大公因數的函式
+// GCD 是Greatest Common Divisor（最大公因數）的縮寫
 long long GCD(long long A, long long B) {
 	long long Answer = 0;
 	for (long long i = 1; i <= min(A, B); i++) {
