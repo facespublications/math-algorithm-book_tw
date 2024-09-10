4ccbacf4696509d9784f367df0a5192cfee73ccf
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:16:42 +0800

    Update Code_4_03_1.c

--- code/c/Code_4_03_1.c
+++ code/c/Code_4_03_1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	double r = 2.0; // √2 を求めたいから
-	double a = 2.0; // 初期値を適当に 2.0 にセットする
+	double r = 2.0; // 因為欲求√2
+	double a = 2.0; // 將初始值任意設定為2.0
 	int i;
 	for (i = 1; i <= 5; i++) {
-		// 点 (a, f(a)) の x 座標と y 座標を求める
+		// 求點(a, f(a)) 的x 座標及y 座標
 		double zahyou_x = a;
 		double zahyou_y = a * a;
 
-		// 接線の傾きを求める [y = (sessen_a)x + sessen_b とする]
+		// 求切線的方程式[ 設為y=(sessen_a)x+sessen_b]
 		double sessen_a = 2.0 * zahyou_x;
 		double sessen_b = zahyou_y - sessen_a * zahyou_x;
 
-		// 次の a の値 next_a を求める
+		// 求下一個a 的值next_a
 		double next_a = (r - sessen_b) / sessen_a;
 		printf("Step #%d: a = %.12lf -> %.12lf\n", i, a, next_a);
 		a = next_a;
