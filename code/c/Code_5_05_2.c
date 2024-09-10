a66d61ec6d50ae5434de166a0e853bb5cbf78821
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:27:34 +0800

    Update Code_5_05_2.c

--- code/c/Code_5_05_2.c
+++ code/c/Code_5_05_2.c
@@ -6,7 +6,7 @@ int check_numpoints(long long lx, long long rx, long long ly, long long ry) {
 	int cnt = 0;
 	int i;
 	for (i = 1; i <= N; i++) {
-		// 点 (X[i], Y[i]) が長方形に含まれているかどうかを判定する
+		// 判定長方形中是否包含點(X[i],Y[i])
 		if (lx <= X[i] && X[i] <= rx && ly <= Y[i] && Y[i] <= ry) {
 			cnt += 1;
 		}
@@ -15,23 +15,23 @@ int check_numpoints(long long lx, long long rx, long long ly, long long ry) {
 }
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d%d", &N, &K);
 	for (int i = 1; i <= N; i++) {
 		scanf("%lld%lld", &X[i], &Y[i]);
 	}
 
-	// 左端 x、右端 x、下端 y、上端 y を全探索（それぞれの番号が i, j, k, l）
-	long long answer = (1LL << 62); // あり得ない値に設定
+	// 將左端x、右端x、下端y 和上端y 進行全搜尋（各自的編號為i,j,k,l）
+	long long answer = (1LL << 62); // 設定成不可能的值
 	int i, j, k, l;
 	for (i = 1; i <= N; i++) {
 		for (j = 1; j <= N; j++) {
 			for (k = 1; k <= N; k++) {
 				for (l = 1; l <= N; l++) {
-					long long cl = X[i]; // 左端の x 座標
-					long long cr = X[j]; // 右端の x 座標
-					long long dl = Y[k]; // 下端の y 座標
-					long long dr = Y[l]; // 上端の y 座標
+					long long cl = X[i]; // 左端的 x 座標
+					long long cr = X[j]; // 右端的 x 座標
+					long long dl = Y[k]; // 下端的 y 座標
+					long long dr = Y[l]; // 上端的 y 座標
 					if (check_numpoints(cl, cr, dl, dr) >= K) {
 						long long area = (cr - cl) * (dr - dl);
 						if (answer > area) {
@@ -43,7 +43,7 @@ int main() {
 		}
 	}
 
-	// 答えの出力
+	// 輸出答案
 	printf("%lld\n", answer);
 	return 0;
 }
