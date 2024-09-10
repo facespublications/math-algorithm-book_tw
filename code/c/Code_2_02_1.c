5c74b1b7abb1fc274c11785d2e2f5cc2cefc8f71
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 12:38:59 +0800

    Update Code_2_02_1.c

--- code/c/Code_2_02_1.c
+++ code/c/Code_2_02_1.c
@@ -3,26 +3,26 @@
 #include <stdlib.h>
 
 int main() {
-	// 四則演算
-	printf("%d\n", 869 + 120); // 989 と出力
-	printf("%d\n", 869 - 120); // 749 と出力
-	printf("%d\n", 869 * 120); // 104280 と出力
-	printf("%d\n", 869 / 120); // 7 と出力（ここでは整数部分のみを出力することに注意）
+	// 四則運算
+	printf("%d\n", 869 + 120); // 輸出 989
+	printf("%d\n", 869 - 120); // 輸出 749
+	printf("%d\n", 869 * 120); // 輸出 104280
+	printf("%d\n", 869 / 120); // 輸出 7 （注意在此只輸出整數部分）
 
-	// 剰余（mod）
-	printf("%d\n", 8 % 5); // 3 と出力
-	printf("%d\n", 869 % 120); // 29 と出力
+	// 餘數（mod）
+	printf("%d\n", 8 % 5); // 輸出 3
+	printf("%d\n", 869 % 120); // 輸出 29
 
-	// 絶対値（abs）（注： C 言語で abs を使うためには <stdlib.h> をインクルード）
-	printf("%d\n", abs(-45)); // 45 と出力
-	printf("%d\n", abs(15)); // 15 と出力
+	// 絕對值（abs）（注： 在 C 語言中藥使用 abs 需 include <stdlib.h>）
+	printf("%d\n", abs(-45)); // 輸出 45
+	printf("%d\n", abs(15)); // 輸出 15
 
-	// 累乗（pow）
-	printf("%d\n", (int)pow(10.0, 2.0)); // 100 と出力
-	printf("%d\n", (int)pow(3.0, 4.0)); // 81 と出力
+	// 乘方（pow）
+	printf("%d\n", (int)pow(10.0, 2.0)); // 輸出 100
+	printf("%d\n", (int)pow(3.0, 4.0)); // 輸出 81
 
-	// ルート（sqrt）
-	printf("%.5lf\n", sqrt(4.0)); // 2.00000 と出力
-	printf("%.5lf\n", sqrt(2.0)); // 1.41421 と出力
+	// 方根（sqrt）
+	printf("%.5lf\n", sqrt(4.0)); // 輸出 2.00000
+	printf("%.5lf\n", sqrt(2.0)); // 輸出 1.41421
 	return 0;
 }
