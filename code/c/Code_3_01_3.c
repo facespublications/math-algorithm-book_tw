f38bdf4050a945a3f92c4d3059b7a729ff318569
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 13:54:51 +0800

    Update Code_3_01_3.c

--- code/c/Code_3_01_3.c
+++ code/c/Code_3_01_3.c
@@ -6,9 +6,9 @@ int main() {
 	long long i;
 	for (i = 1; i * i <= N; i++) {
 		if (N % i != 0) continue;
-		printf("%lld\n", i); // i を約数に追加
+		printf("%lld\n", i); // 追加因數 i
 		if (i != N / i) {
-			printf("%lld\n", N / i); // i ≠ N/i のとき、N/i も約数に追加
+			printf("%lld\n", N / i); // i ≠ N/i時，也追加因數 N/i
 		}
 	}
 	return 0;
