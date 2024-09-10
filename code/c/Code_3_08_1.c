65b746a46835a9fcf6fd499fa1e7c30f08c96dc6
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:13:57 +0800

    Update Code_3_08_1.c

--- code/c/Code_3_08_1.c
+++ code/c/Code_3_08_1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int compare_values(const void* a, const void* b) {
-	// 2 つの long long 型の値を比較する関数（Code_3_06_1.c を参照）
+	// 比較 2 個 long long 型的值的函數（參考 Code_3_06_1.c）
 	if (*(long long*)a < *(long long*)b) return -1;
 	if (*(long long*)a > *(long long*)b) return +1;
 	return 0;
@@ -11,29 +11,29 @@ int compare_values(const void* a, const void* b) {
 int N; long long X, A[1000009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d%lld", &N, &X);
 	int i;
 	for (i = 1; i <= N; i++) {
 		scanf("%lld", &A[i]);
 	}
 
-	// 配列のソート（Code_3_06_1.c を参照）
+	// 陣列的排序（參考 Code_3_06_1.c）
 	qsort(A + 1, N, sizeof(long long), compare_values);
 
-	// 二分探索
+	// 二元搜尋
 	int left = 1, right = N;
 	while (left <= right) {
-		int mid = (left + right) / 2; // 探索範囲の中央で分割する
+		int mid = (left + right) / 2; // 以搜尋範圍的中央進行分割
 		if (A[mid] == X) {
 			printf("Yes\n");
 			return 0;
 		}
-		if (A[mid] > X) right = mid - 1; // 探索範囲を前半部分に絞る
-		if (A[mid] < X) left = mid + 1; // 探索範囲を後半部分に絞る
+		if (A[mid] > X) right = mid - 1; // 將搜尋範圍縮小到前半部分
+		if (A[mid] < X) left = mid + 1; // 將搜尋範圍縮小到後半部分
 	}
 
-	// 探索範囲がなくなっても Yes とならなければ答えは No
+	// 當搜尋範圍耗盡仍非Yes 時，答案即為No
 	printf("No\n");
 	return 0;
 }
