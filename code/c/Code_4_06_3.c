6b10d24c067a85033b52365936b169b0d104a801
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 15:09:00 +0800

    Update Code_4_06_3.c

--- code/c/Code_4_06_3.c
+++ code/c/Code_4_06_3.c
@@ -4,7 +4,7 @@ int main() {
 	long long a, b;
 	scanf("%lld%lld", &a, &b);
 	const long long MOD = 1000000007;
-	long long answer = 1; // a の 0 乗は 1 なので、Answer=1 に初期化しておく
+	long long answer = 1; // a 的 0 次方為 1，因此初始化成 Answer=1
 	long long i;
 	for (i = 1; i <= b; i++) {
 		answer = (answer * a) % MOD;
