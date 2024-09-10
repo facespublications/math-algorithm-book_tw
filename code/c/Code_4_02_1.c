faa0e5614515810ba01f2b7ed0fb219342b3458f
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-10 14:15:01 +0800

    Update Code_4_02_1.c

--- code/c/Code_4_02_1.c
+++ code/c/Code_4_02_1.c
@@ -4,7 +4,7 @@ int N, A[100009], B[100009];
 int Q, L[100009], R[100009];
 
 int main() {
-	// 入力
+	// 輸入
 	scanf("%d%d", &N, &Q);
 	int i, j;
 	for (i = 1; i <= N; i++) {
@@ -14,13 +14,13 @@ int main() {
 		scanf("%d%d", &L[j], &R[j]);
 	}
 	
-	// 累積和を求める
+	// 求出累積和
 	B[0] = 0;
 	for (i = 1; i <= N; i++) {
 		B[i] = B[i - 1] + A[i];
 	}
 
-	// 答えの出力
+	// 輸出答案
 	for (j = 1; j <= Q; j++) {
 		printf("%d\n", B[R[j]] - B[L[j] - 1]);
 	}
