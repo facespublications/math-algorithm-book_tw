ed4c4b6d625916f23446a6c22c7cdb7f641d387b
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:12:40 +0800

    Update Code_2_01_4.java

--- code/java/Code_2_01_4.java
+++ code/java/Code_2_01_4.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Code_2_01_4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 入力部分
-		String answer = ""; // String は文字列型
+		int N = sc.nextInt(); // 輸入部分
+		String answer = ""; // String 為字串型別
 		while (N >= 1) {
-			// N % 2 は N を 2 で割った余り（例：N = 13 の場合 1）
-			// N / 2 は N を 2 で割った値の整数部分（例：N = 13 の場合 6）
+			// N % 2 為 N 除以 2 的餘數（例如：N=13 時為1）
+			// N / 2 為 N 除以 2 的值的整數部分（例如：N=13 時為6）
 			if (N % 2 == 0) {
 				answer = "0" + answer;
 			}
@@ -16,6 +16,6 @@ class Code_2_01_4 {
 			}
 			N = N / 2;
 		}
-		System.out.println(answer); // 出力部分
+		System.out.println(answer); // 輸出部分
 	}
 };
