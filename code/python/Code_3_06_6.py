a82d01a7c507cbd97272f4a195bf3a2997d36bf2
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:07:10 +0800

    Update Code_3_06_6.py

--- code/python/Code_3_06_6.py
+++ code/python/Code_3_06_6.py
@@ -1,5 +1,5 @@
 def func(N):
-	# func(N) → func(N-1) → ... → func(0) → func(-1) → func(-2) → ... と無限に呼び出されるので、プログラムが正常に動作しない
+	# func(N) → func(N-1) → ... → func(0) → func(-1) → func(-2) → ... 會像這樣無限呼叫，程式無法正常運作
 	return func(N - 1) * N
 
 N = int(input())
