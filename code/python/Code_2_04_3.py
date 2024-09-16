4257f83c850c34557b673d0aa4106639c99e87c5
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:47:39 +0800

    Update Code_2_04_3.py

--- code/python/Code_2_04_3.py
+++ code/python/Code_2_04_3.py
@@ -1,12 +1,12 @@
-# 入力
+# 輸入
 N, S = map(int, input().split())
 
-# 答えを求める
+# 求出答案
 answer = 0
 for i in range(1, N + 1):
 	for j in range(1, N + 1):
 		if i + j <= S:
 			answer += 1
 
-# 出力
+# 輸出
 print(answer)
