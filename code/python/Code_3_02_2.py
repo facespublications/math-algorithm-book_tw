8ee739c5527eccbe31264ca654a5bbf888d0f023
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:53:57 +0800

    Update Code_3_02_2.py

--- code/python/Code_3_02_2.py
+++ code/python/Code_3_02_2.py
@@ -1,11 +1,11 @@
-# 正の整数 A と B の最大公約数を返す関数
-# GCD は Greatest Common Divisor（最大公約数）の略
+# 返回正整數 A 和 B 的最大公因數的函式
+# GCD 是Greatest Common Divisor（最大公因數）的縮寫
 def GCD(A, B):
 	while A >= 1 and B >= 1:
 		if A < B:
-			B = B % A  # A < B の場合、大きい方 B を書き換える
+			B = B % A  # A < B時，改寫較大的數B
 		else:
-			A = A % B  # A >= B の場合、大きい方 A を書き換える
+			A = A % B  # A >= B時，改寫較大的數A
 	if A >= 1:
 		return A
 	return B
