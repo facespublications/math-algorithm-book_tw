c4adad0b358c0925680b8bb4666aeb86340c87c1
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:14:11 +0800

    Update Code_4_01_1.py

--- code/python/Code_4_01_1.py
+++ code/python/Code_4_01_1.py
@@ -1,24 +1,24 @@
 import math
 
-# 入力
+# 輸入
 ax, ay = map(int, input().split())
 bx, by = map(int, input().split())
 cx, cy = map(int, input().split())
 
-# ベクトル BA, BC, CA, CB の成分表示を求める
+# 求向量BA、BC、CA、CB 的分量表示
 BAx, BAy = ax - bx, ay - by
 BCx, BCy = cx - bx, cy - by
 CAx, CAy = ax - cx, ay - cy
 CBx, CBy = bx - cx, by - cy
 
-# どのパターンに当てはまるか判定する
+# 判斷適用哪一個模式
 pattern = 2
 if BAx * BCx + BAy * BCy < 0:
 	pattern = 1
 if CAx * CBx + CAy * CBy < 0:
 	pattern = 3
 
-# 点と直線の距離を求める
+# 求出點與直線的距離
 if pattern == 1:
 	answer = math.sqrt(BAx ** 2 + BAy ** 2)
 if pattern == 3:
@@ -28,5 +28,5 @@ if pattern == 2:
 	BCLength = math.sqrt(BCx ** 2 + BCy ** 2)
 	answer = S / BCLength
 
-# 答えの出力
+# 輸出答案
 print("%.12f" % answer)
