4b8baf777f7eb200e0b2f4917338d17af6add44a
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:09:14 +0800

    Update Code_3_07_1.java

--- code/java/Code_3_07_1.java
+++ code/java/Code_3_07_1.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_3_07_1 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int[] H = new int[N + 1];
@@ -10,7 +10,7 @@ class Code_3_07_1 {
 			H[i] = sc.nextInt();
 		}
 		
-		// 動的計画法
+		// 動態規劃法
 		int[] dp = new int[N + 1];
 		dp[1] = 0;
 		for (int i = 2; i <= N; i++) {
@@ -24,7 +24,7 @@ class Code_3_07_1 {
 			}
 		}
 		
-		// 答えの出力
+		// 輸出答案
 		System.out.println(dp[N]);
 	}
 }
