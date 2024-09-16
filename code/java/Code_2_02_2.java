a5d5c941ec73c1e6879780447af5372d790242b1
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:14:29 +0800

    Update Code_2_02_2.java

--- code/java/Code_2_02_2.java
+++ code/java/Code_2_02_2.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Code_2_02_2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt(); // a を入力する
-		int b = sc.nextInt(); // b を入力する
-		System.out.println(a & b); // a AND b の値を出力する
-		System.out.println(a | b); // a OR b の値を出力する
-		System.out.println(a ^ b); // a XOR b の値を出力する
+		int a = sc.nextInt(); // 輸入 a
+		int b = sc.nextInt(); // 輸入 b
+		System.out.println(a & b); // 輸出 a AND b的值
+		System.out.println(a | b); // 輸出 a OR b的值
+		System.out.println(a ^ b); // 輸出 a XOR b的值
 	}
 };
