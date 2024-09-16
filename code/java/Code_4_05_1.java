8f820a8cd54e420ce90ffd5546093a975096fde2
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:42:52 +0800

    Update Code_4_05_1.java

--- code/java/Code_4_05_1.java
+++ code/java/Code_4_05_1.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_4_05_1 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
@@ -13,24 +13,24 @@ class Code_4_05_1 {
 			B[i] = sc.nextInt();
 		}
 		
-		// 隣接リストの作成
+		// 製作鄰接表
 		ArrayList<Integer>[] G = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
-			G[i] = new ArrayList<Integer>(); // G[i] は頂点 i に隣接する頂点のリスト
+			G[i] = new ArrayList<Integer>(); // G[i] 是與頂點 i 相鄰頂點的列表
 		}
 		for (int i = 1; i <= M; i++) {
-			G[A[i]].add(B[i]); // 頂点 A[i] に隣接する頂点として B[i] を追加
-			G[B[i]].add(A[i]); // 頂点 B[i] に隣接する頂点として A[i] を追加
+			G[A[i]].add(B[i]); // 添加 B[i] 作為與頂點 A[i] 相鄰的頂點
+			G[B[i]].add(A[i]); // 添加 A[i] 作為與頂點 B[i] 相鄰的頂點
 		}
 		
-		// 出力（G[i].size() は頂点 i に隣接する頂点のリストの大きさ ＝ 次数）
+		// 輸出（G[i].size() 為與頂點i 相鄰頂點的列表的大小 = 度）
 		for (int i = 1; i <= N; i++) {
 			System.out.format("%d: {", i);
 			for (int j = 0; j < G[i].size(); j++) {
 				if (j >= 1) {
 					System.out.print(",");
 				}
-				System.out.print(G[i].get(j)); // G[i].get(j) は頂点 i に隣接する頂点のうち j+1 番目のもの
+				System.out.print(G[i].get(j)); // G[i].get(j) 是與頂點　i 相鄰頂點之中的第 j+1 個頂點
 			}
 			System.out.println("}");
 		}
