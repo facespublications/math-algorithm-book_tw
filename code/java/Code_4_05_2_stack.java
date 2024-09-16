4a721a1d49c63b1b542feb0aa74fea8cccdbfed9
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:48:22 +0800

    Update Code_4_05_2_stack.java

--- code/java/Code_4_05_2_stack.java
+++ code/java/Code_4_05_2_stack.java
@@ -1,13 +1,13 @@
-// このソースコードは、深さ優先探索 (DFS) をスタックを用いて実装したものです。
-// スタックは「一番上に要素を積む」「一番上の要素を調べる」「一番上に積まれた要素を取り除く」という 3 種類の操作ができるデータ構造です。
-// 深さ優先探索の部分は、コード 4.5.3 の queue を stack に変更したものをベースに書かれています。
+// 這個原始碼為將深度優先搜尋 (DFS) 使用了堆疊的實作例。
+// 堆疊為可進行「在最上層堆積元素」、「查找堆積在最上層的元素」、「刪除堆積在最上層的元素」三種操作的資料結構。
+// 深度優先搜尋的部分為將程式碼 4.5.3 的 queue 變更為 stack，以此為基礎撰寫。
 
 import java.util.*;
 import java.io.*;
 
 class Main {
 	public static void main(String[] args) throws IOException {
-		// 入力（高速な入出力のため、Scanner の代わりに BufferedReader を使っています）
+		// 輸入（由於是高速的輸入輸出，使用 BufferedReader 代替 Scanner）
 		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(buff.readLine());
@@ -21,7 +21,7 @@ class Main {
 			B[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 隣接リストの作成
+		// 製作鄰接表
 		ArrayList<Integer>[] G = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
 			G[i] = new ArrayList<Integer>();
@@ -31,27 +31,27 @@ class Main {
 			G[B[i]].add(A[i]);
 		}
 		
-		// 深さ優先探索の初期化
+		// 深度優先搜尋的初始化
 		boolean[] visited = new boolean[N + 1];
 		for (int i = 1; i <= N; i++) {
 			visited[i] = false;
 		}
-		Stack<Integer> S = new Stack<>(); // スタック S を定義する
+		Stack<Integer> S = new Stack<>(); // 定義堆疊 S 
 		visited[1] = true;
-		S.push(1); // S に 1 を追加
+		S.push(1); // S 追加 1
 
-		// 深さ優先探索
+		// 深度優先搜尋
 		while (S.size() >= 1) {
-			int pos = S.pop(); // S の先頭を調べ、これを取り出す
+			int pos = S.pop(); // 調查 S 的開頭並將之取出
 			for (int nex : G[pos]) {
 				if (visited[nex] == false) {
 					visited[nex] = true;
-					S.push(nex); // S に nex を追加
+					S.push(nex); // 追加 nex 至 S
 				}
 			}
 		}
 		
-		// 連結かどうかの判定（answer = true のとき連結）
+		// 判定是否連通（answer = true 時為連通）
 		boolean answer = true;
 		for (int i = 1; i <= N; i++) {
 			if (visited[i] == false) {
