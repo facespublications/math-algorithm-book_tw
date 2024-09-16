f79c92501b6a35f79d72aa3d9d765987c33a3d9e
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:02:15 +0800

    Update Code_3_06_5.py

--- code/python/Code_3_06_5.py
+++ code/python/Code_3_06_5.py
@@ -1,15 +1,15 @@
 def solve(l, r, A):
 	if r - l == 1:
 		return A[l]
-	m = (l + r) // 2  # 区間 [l, r) の中央で分割する
-	s1 = solve(l, m, A)  # s1 は A[l] + ... + A[m-1] の合計値となる
-	s2 = solve(m, r, A)  # s2 は A[m] + ... + A[r-1] の合計値となる
+	m = (l + r) // 2  # 在區間 [l,r)的中央進行分割
+	s1 = solve(l, m, A)  # s1為 A[l]+...+A[m-1]的合計值
+	s2 = solve(m, r, A)  # s2為 A[m]+...+A[r-1] 的合計值
 	return s1 + s2
 
-# 入力
+# 輸入
 N = int(input())
 A = list(map(int, input().split()))
 
-# 再帰呼び出し → 答えの出力
+# 遞迴呼叫 → 輸出答案
 answer = solve(0, N, A)
 print(answer)
