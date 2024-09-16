9cb90a905601e32b2dc5ec6686ee02d94b64d3c2
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:36:23 +0800

    Update Code_4_04_1.java

--- code/java/Code_4_04_1.java
+++ code/java/Code_4_04_1.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_4_04_1 {
 	public static void main(String[] args) {
-		// 入力 → 配列 prime の初期化
+		// 輸入 → 陣列 prime 的初始化
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		boolean[] prime = new boolean[N + 1];
@@ -10,7 +10,7 @@ class Code_4_04_1 {
 			prime[i] = true;
 		}
 		
-		// エラトステネスのふるい
+		// 埃拉托斯特尼篩
 		for (int i = 2; i * i <= N; i++) {
 			if (prime[i] == true) {
 				for (int x = 2 * i; x <= N; x += i) {
@@ -19,7 +19,7 @@ class Code_4_04_1 {
 			}
 		}
 		
-		// N 以下の素数を小さい順に出力
+		// 將 N 以下的質數以從小到大的順序輸出
 		for (int i = 2; i <= N; i++) {
 			if (prime[i] == true) {
 				System.out.println(i);
