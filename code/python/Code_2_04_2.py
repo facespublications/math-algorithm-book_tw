f5fb90790c7aac94ebe2be5d15c17a8eb459e29d
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:47:21 +0800

    Update Code_2_04_2.py

--- code/python/Code_2_04_2.py
+++ code/python/Code_2_04_2.py
@@ -1,12 +1,12 @@
-# 入力
+# 輸入
 N, X, Y = map(int, input().split())
 
-# 答えを求める
+# 求出答案
 cnt = 0
 for i in range(1, N + 1):
-	# mod の計算は 2.2 節参照
+	# mod 的計算請參考 2.2 節
 	if i % X == 0 or i % Y == 0:
 		cnt += 1
 
-# 出力
+# 輸出
 print(cnt)
