9289d704974a887393a5e9a2aea6780505fbf8ed
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:21:22 +0800

    Update Code_3_04_2.java

--- code/java/Code_3_04_2.java
+++ code/java/Code_3_04_2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_3_04_2 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int[] P = new int[N + 1];
@@ -12,7 +12,7 @@ class Code_3_04_2 {
 			Q[i] = sc.nextInt();
 		}
 		
-		// 答えの計算・出力
+		// 答案的計算、輸出
 		double answer = 0.0;
 		for (int i = 1; i <= N; i++) {
 			answer += (double)Q[i] / P[i];
