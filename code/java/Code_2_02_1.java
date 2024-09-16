837c63ceb1290ad5d99aef87ec90846c58523f3c
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:13:56 +0800

    Update Code_2_02_1.java

--- code/java/Code_2_02_1.java
+++ code/java/Code_2_02_1.java
@@ -3,25 +3,25 @@ import java.util.*;
 class Code_2_02_1 {
 	public static void main(String[] args) {
 		// 四則演算
-		System.out.println(869 + 120); // 989 と出力
-		System.out.println(869 - 120); // 749 と出力
-		System.out.println(869 * 120); // 104280 と出力
-		System.out.println(869 / 120); // 7 と出力（整数型同士の割り算では、整数に切り捨てられることに注意）
+		System.out.println(869 + 120); // 輸出 989
+		System.out.println(869 - 120); // 輸出 749
+		System.out.println(869 * 120); // 輸出 104280
+		System.out.println(869 / 120); // 輸出 7 （注意在此只輸出整數部分）
 		
-		// 剰余 (mod)
-		System.out.println(8 % 5); // 3 と出力
-		System.out.println(869 % 120); // 29 と出力
+		// 餘數 (mod)
+		System.out.println(8 % 5); // 輸出 3
+		System.out.println(869 % 120); // 輸出 29
 		
-		// 絶対値 (abs)
-		System.out.println(Math.abs(-45)); // 45 と出力
-		System.out.println(Math.abs(15)); // 15 と出力
+		// 絕對值 (abs)
+		System.out.println(Math.abs(-45)); // 輸出 45
+		System.out.println(Math.abs(15)); // 輸出 15
 		
-		// 累乗 (pow)
-		System.out.println((int)Math.pow(10.0, 2.0)); // 100 と出力
-		System.out.println((int)Math.pow(3.0, 4.0)); // 81 と出力
+		// 乘方 (pow)
+		System.out.println((int)Math.pow(10.0, 2.0)); // 輸出 100
+		System.out.println((int)Math.pow(3.0, 4.0)); // 輸出 81
 		
-		// 平方根 (sqrt)
-		System.out.format("%.5f\n", Math.sqrt(4.0)); // 2.00000 と出力
-		System.out.format("%.5f\n", Math.sqrt(2.0)); // 1.41421 と出力
+		// 方根 (sqrt)
+		System.out.format("%.5f\n", Math.sqrt(4.0)); // 輸出 2.00000
+		System.out.format("%.5f\n", Math.sqrt(2.0)); // 輸出 1.41421
 	}
 };
