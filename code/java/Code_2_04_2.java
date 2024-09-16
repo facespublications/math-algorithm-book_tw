e46a0c78ca254e3c424a86b539b47d616cff49ea
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:15:16 +0800

    Update Code_2_04_2.java

--- code/java/Code_2_04_2.java
+++ code/java/Code_2_04_2.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Code_2_04_2 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int X = sc.nextInt();
 		int Y = sc.nextInt();
 		
-		// 答えを求める
+		// 求解
 		int cnt = 0;
 		for (int i = 1; i <= N; i++) {
-			// mod の計算は 2.2 節参照
+			// mod 的計算請參考 2.2 節
 			if (i % X == 0 || i % Y == 0) {
 				cnt++;
 			}
 		}
 		
-		// 出力
+		// 輸出
 		System.out.println(cnt);
 	}
 }
