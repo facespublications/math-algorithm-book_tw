3b8714e6273770609c0e336e41165a4f7076a502
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:10:08 +0800

    Update Code_3_07_3.py

--- code/python/Code_3_07_3.py
+++ code/python/Code_3_07_3.py
@@ -1,31 +1,31 @@
 # 注意
-# Python は配列のランダムアクセスに時間がかかるため、N = 100, W = 100000 では実行に 5 秒程度かかり、TLE（実行時間制限オーバー）になります。
-# 一方、同じプログラムを PyPy3 で提出すると、実行に 0.5 秒程度しかかからず、AC（正解）することができます。
+# 由於 Python 進行陣列的隨機訪問會花費較多時間，因此當 N = 100，W = 100000 時，執行時間大約需要 5 秒，可能會 TLE（超過執行時間限制）。
+# 另一方面，如果將相同的程式提交到 PyPy3，執行時間只需要大約 0.5 秒，並且能夠 AC（通過測試）。
 
-# 入力
+# 輸入
 N, W = map(int, input().split())
 w = [ None ] * N
 v = [ None ] * N
 for i in range(N):
 	w[i], v[i] = map(int, input().split())
 
-# 配列の初期化
+# 陣列的初始化
 INF = 10 ** 18
 dp = [ [ None ] * (W + 1) for i in range(N + 1) ]
 dp[0][0] = 0
 for i in range(1, W + 1):
 	dp[0][i] = -INF
 
-# 動的計画法
+# 動態規劃法
 for i in range(1, N + 1):
 	for j in range(0, W + 1):
 		if j < w[i - 1]:
-			# j < w[i-1] のとき、方法 A だけしか選べない
+			# j < w[i-1] 時，只能選擇方法 A
 			dp[i][j] = dp[i - 1][j]
 		else:
-			# j >= w[i-1] のとき、方法 A・方法 B どちらも選べる
+			# j >= w[i-1] 時，可以選擇方法 A、方法 B 的任一個
 			dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + v[i - 1])
 
-# 答えを計算して出力
+# 計算並輸出答案
 answer = max(dp[N])
 print(answer)
