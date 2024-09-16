b88dbba683185367603c28482dbcd3df548b0463
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:08:37 +0800

    Update Code_3_07_2.py

--- code/python/Code_3_07_2.py
+++ code/python/Code_3_07_2.py
@@ -1,7 +1,7 @@
-# 入力
+# 輸入
 N = int(input())
 
-# 動的計画法
+# 動態規劃法
 dp = [ None ] * (N + 1)
 for i in range(N + 1):
 	if i <= 1:
@@ -9,5 +9,5 @@ for i in range(N + 1):
 	else:
 		dp[i] = dp[i - 1] + dp[i - 2]
 
-# 答えの出力
+# 輸出答案
 print(dp[N])
