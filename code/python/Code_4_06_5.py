dbc76096e4cfcac1f0e7dd964e9807c786d312a7
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:24:55 +0800

    Update Code_4_06_5.py

--- code/python/Code_4_06_5.py
+++ code/python/Code_4_06_5.py
@@ -1,4 +1,4 @@
-# 繰り返し二乗法（p は a**1, a**2, a**4, a**8, ... といった値をとる）
+# 重複平方法（p 取 a^1、a^2、 a^4、a^8、⋯的值）
 def modpow(a, b, m):
 	p = a
 	answer = 1
@@ -8,16 +8,16 @@ def modpow(a, b, m):
 		p = (p * p) % m
 	return answer
 
-# division(a, b, m) は a÷b mod m を返す関数
+# division(a, b, m) 是回傳　a÷b mod m 的函式
 def division(a, b, m):
 	return (a * modpow(b, m - 2, m)) % m
 
 MOD = 1000000007
 
-# 入力
+# 輸入
 X, Y = map(int, input().split())
 
-# 二項係数の分子と分母を求める（手順 1.／手順 2.）
+# 求出二項係數的分子和分母（步驟1./ 步驟2.）
 bunshi, bunbo = 1, 1
 for i in range(1, X + Y + 1):
 	bunshi = (bunshi * i) % MOD
