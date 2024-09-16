64f4b725a0efcb411f6e423df0e5f5cd38e4ab45
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:28:31 +0800

    Update Code_5_04_1.py

--- code/python/Code_5_04_1.py
+++ code/python/Code_5_04_1.py
@@ -1,15 +1,15 @@
-# 入力
+# 輸入
 N, K = map(int, input().split())
 
-# 事象 B の個数 yojishou を数える
+# 算出事件 B 的個數 yojishou
 yojishou = 0
 for a in range(1, N + 1):
-	l = max(1, a - (K - 1)) # b, c の探索範囲の下限 l
-	r = min(N, a + (K - 1)) # b, c の探索範囲の上限 r
+	l = max(1, a - (K - 1)) # b, c 的搜尋範圍下限 l
+	r = min(N, a + (K - 1)) # b, c 的搜尋範圍上限 r
 	for b in range(l, r + 1):
 		for c in range(l, r + 1):
 			if abs(b - c) <= K - 1:
 				yojishou += 1
 
-# 答えの出力
+# 輸出答案
 print(N ** 3 - yojishou)
