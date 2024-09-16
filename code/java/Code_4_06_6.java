c5ff146a25f187792619a66f54acf78bb9b00f30
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:00:28 +0800

    Update Code_4_06_6.java

--- code/java/Code_4_06_6.java
+++ code/java/Code_4_06_6.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Code_4_06_6 {
 	public static void main(String[] args) {
-		// 配列 fact の初期化（fact[i] は i の階乗を 1000000007 で割った余り）
+		// 陣列初始化（fact[i] 是i 的階乘除以10000007 的餘數）
 		fact = new long[LIMIT + 1];
 		fact[0] = 1;
 		for (int i = 1; i <= LIMIT; i++) {
 			fact[i] = fact[i - 1] * i % MOD;
 		}
 		
-		// 入力 → 答えの出力
+		// 輸入→答案輸出
 		Scanner sc = new Scanner(System.in);
 		int X = sc.nextInt();
 		int Y = sc.nextInt();
@@ -19,7 +19,7 @@ class Code_4_06_6 {
 	static final int LIMIT = 200000;
 	static long[] fact;
 	static long modpow(long a, long b, long m) {
-		// 繰り返し二乗法（p は a^1, a^2, a^4, a^8, ... といった値をとる）
+		// 重複平方法（p 取 a^1、a^2、 a^4、a^8、⋯的值）
 		long p = a, answer = 1;
 		for (int i = 0; i < 30; i++) {
 			if ((b & (1 << i)) != 0) {
@@ -30,7 +30,7 @@ class Code_4_06_6 {
 		return answer;
 	}
 	static long division(long a, long b, long m) {
-		// division(a, b, m) は a÷b mod m を返す関数
+		// division(a, b, m) 是求 a÷b mod m 的函式
 		return (a * modpow(b, m - 2, m)) % m;
 	}
 	static long ncr(int n, int r) {
