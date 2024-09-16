57cd76eeceac30a1b362673f8dbfee59e37377f9
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:59:29 +0800

    Update Code_4_06_5.java

--- code/java/Code_4_06_5.java
+++ code/java/Code_4_06_5.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 class Code_4_06_5 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int X = sc.nextInt();
 		int Y = sc.nextInt();
 		
-		// 二項係数の分子と分母を求める（手順 1.／手順 2.）
+		// 求出二項係數的分子和分母（步驟1./ 步驟2.）
 		final long MOD = 1000000007;
 		long bunshi = 1, bunbo = 1;
 		for (int i = 1; i <= X + Y; i++) {
@@ -20,12 +20,12 @@ class Code_4_06_5 {
 			bunbo = (bunbo * i) % MOD;
 		}
 		
-		// 答えを求める（手順 3.）
+		// 求出答案（步驟3.）
 		long answer = division(bunshi, bunbo, MOD);
 		System.out.println(answer);
 	}
 	static long modpow(long a, long b, long m) {
-		// 繰り返し二乗法（p は a^1, a^2, a^4, a^8, ... といった値をとる）
+		// 重複平方法（p 取 a^1、a^2、 a^4、a^8、⋯的值）
 		long p = a, answer = 1;
 		for (int i = 0; i < 30; i++) {
 			if ((b & (1 << i)) != 0) {
@@ -36,7 +36,7 @@ class Code_4_06_5 {
 		return answer;
 	}
 	static long division(long a, long b, long m) {
-		// division(a, b, m) は a÷b mod m を返す関数
+		// division(a, b, m) 是回傳　a÷b mod m 的函式
 		return (a * modpow(b, m - 2, m)) % m;
 	}
 }
