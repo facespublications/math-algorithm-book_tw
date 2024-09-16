6c426b4e1f29021153203784001d801bf28ff861
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:32:53 +0800

    Update Code_5_07_2.py

--- code/python/Code_5_07_2.py
+++ code/python/Code_5_07_2.py
@@ -1,4 +1,4 @@
-# 繰り返し二乗法（p は a**1, a**2, a**4, a**8, ... といった値をとる）
+# 重複平方法（p 取 a^1、a^2、 a^4、a^8、⋯的值）
 def modpow(a, b, m):
 	p = a
 	answer = 1
@@ -8,11 +8,11 @@ def modpow(a, b, m):
 		p = (p * p) % m
 	return answer
 
-# division(a, b, m) は a÷b mod m を返す関数
+# division(a, b, m) 為回傳 a÷b mod m 的函式
 def division(a, b, m):
 	return (a * modpow(b, m - 2, m)) % m
 
-# ncr は n! を r! × (n-r)! で割った値
+# ncr 是 n! 除以 r!×(n-r)! 的值
 def ncr(n, r):
 	global fact, MOD
 	return division(fact[n], fact[r] * fact[n - r] % MOD, MOD)
@@ -20,21 +20,21 @@ def ncr(n, r):
 MOD = 1000000007
 LIMIT = 200000
 
-# 配列 fact の初期化（fact[i] は i の階乗を 1000000007 で割った余り）
+# 陣列化 fact 的初始（fact[i] 是i 的階乘除以10000007 的餘數）
 fact = [ None ] * (LIMIT + 1)
 fact[0] = 1
 for i in range(1, LIMIT + 1):
 	fact[i] = fact[i - 1] * i % MOD
 
-# 入力
+# 輸入
 N = int(input())
 A = list(map(int, input().split()))
 
-# 答えを求める
+# 求出答案
 answer = 0
 for i in range(N):
 	answer += A[i] * ncr(N - 1, i)
 	answer %= MOD
 
-# 答えの出力
+# 輸出答案
 print(answer)
