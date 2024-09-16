c82068a6933330a16e0a9971a83f5b2abbb6c069
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:31:33 +0800

    Update Code_5_10_3.java

--- code/java/Code_5_10_3.java
+++ code/java/Code_5_10_3.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Code_5_10_3 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		long N = sc.nextLong();
 		long X = sc.nextLong();
 		long Y = sc.nextLong();
 		
-		// 4 つの整数 (a, b, c, d) の全探索
+		// 4 個整數(a, b, c, d) 的全搜尋
 		boolean flag = false;
 		for (int a = 1; a <= N; a++) {
 			for (int b = a; b <= N; b++) {
 				for (int c = b; c <= N; c++) {
 					for (int d = c; d <= N; d++) {
 						if (a + b + c + d == X && (long)a * b * c * d == Y) {
-							flag = true; // 答えが見つかったら flag を true にする
+							flag = true; // 如果沒找到答案的話 flag 為 true 
 						}
 					}
 				}
 			}
 		}
 		
-		// 答えの出力
+		// 輸出答案
 		if (flag == true) {
 			System.out.println("Yes");
 		}
