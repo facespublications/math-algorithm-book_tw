fbf0af0c86a099f7082cb1dc0d9b560106ddf5a3
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:19:23 +0800

    Update Code_3_02_1.java

--- code/java/Code_3_02_1.java
+++ code/java/Code_3_02_1.java
@@ -8,8 +8,8 @@ class Code_3_02_1 {
 		System.out.println(GCD(A, B));
 	}
 	static long GCD(long A, long B) {
-		// 正の整数 A と B の最大公約数を返す関数
-		// GCD は Greatest Common Divisor（最大公約数）の略
+		// 返回正整數 A 和 B 的最大公因數的函式
+		// GCD 是Greatest Common Divisor（最大公因數）的縮寫
 		long ans = 0;
 		for (long i = 1; i <= A && i <= B; i++) {
 			if (A % i == 0 && B % i == 0) {
