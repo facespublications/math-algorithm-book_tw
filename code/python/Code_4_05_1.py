f914a16f8f30ef43692ca0c7cd4036e959897a89
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:17:23 +0800

    Update Code_4_05_1.py

--- code/python/Code_4_05_1.py
+++ code/python/Code_4_05_1.py
@@ -1,22 +1,22 @@
-# 入力
+# 輸入
 N, M = map(int, input().split())
 A = [ None ] * M
 B = [ None ] * M
 for i in range(M):
 	A[i], B[i] = map(int, input().split())
 
-# 隣接リストの作成
-G = [ list() for i in range(N + 1) ] # G[i] は頂点 i に隣接する頂点のリスト
+# 製作鄰接表
+G = [ list() for i in range(N + 1) ] # G[i] 是與頂點 i 相鄰頂點的列表
 for i in range(M):
-	G[A[i]].append(B[i]) # 頂点 A[i] に隣接する頂点として B[i] を追加
-	G[B[i]].append(A[i]) # 頂点 B[i] に隣接する頂点として A[i] を追加
+	G[A[i]].append(B[i]) # 添加 B[i] 作為與頂點 A[i] 相鄰的頂點
+	G[B[i]].append(A[i]) # 添加 A[i] 作為與頂點 B[i] 相鄰的頂點
 
-# 出力（len(G[i]) は頂点 i に隣接する頂点のリストの大きさ ＝ 次数）
+# 輸出（len(G[i]) 為與頂點i 相鄰頂點的列表的大小 = 度）
 for i in range(1, N + 1):
 	output = str(i) + ": {"
 	for j in range(len(G[i])):
 		if j >= 1:
 			output += ","
-		output += str(G[i][j]) # G[i][j] は頂点 i に隣接する頂点のうち j+1 番目のもの
+		output += str(G[i][j]) # G[i][j] 是與頂點　i 相鄰頂點之中的第 j+1 個頂點
 	output += "}"
 	print(output)
