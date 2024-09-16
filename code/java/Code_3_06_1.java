8ad117c8dbf4ff153e817e753161f79aa83d6609
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:24:59 +0800

    Update Code_3_06_1.java

--- code/java/Code_3_06_1.java
+++ code/java/Code_3_06_1.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_3_06_1 {
 	public static void main(String[] args) {
-		// 入力（たとえば N = 5, A = [3, 1, 4, 1, 5] を入力したとする）
+		// 輸入（例如輸入了 N = 5, A = [3, 1, 4, 1, 5] ）
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int[] A = new int[N];
@@ -10,10 +10,10 @@ class Code_3_06_1 {
 			A[i - 1] = sc.nextInt();
 		}
 		
-		// 配列 A 全体をソートする
+		// 排序陣列 A 整體
 		Arrays.sort(A);
 		
-		// 出力（1, 1, 3, 4, 5 の順に出力される）
+		// 輸出（按照1、1、3、4、5 的順序輸出）
 		for (int i = 1; i <= N; i++) {
 			System.out.println(A[i - 1]);
 		}
