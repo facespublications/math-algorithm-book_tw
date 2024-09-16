80187360f2eb9b055d8031c983593fdf84051aec
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:11:33 +0800

    Update Code_5_04_1.java

--- code/java/Code_5_04_1.java
+++ code/java/Code_5_04_1.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 class Code_5_04_1 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int K = sc.nextInt();
-		// 事象 B の個数 yojishou を数える
+		// 算出事件 B 的個數 yojishou
 		long yojishou = 0;
 		for (int a = 1; a <= N; a++) {
-			int l = Math.max(1, a - (K - 1)); // b, c の探索範囲の下限 l
-			int r = Math.min(N, a + (K - 1)); // b, c の探索範囲の上限 r
+			int l = Math.max(1, a - (K - 1)); // b, c 的搜尋範圍下限 l
+			int r = Math.min(N, a + (K - 1)); // b, c 的搜尋範圍上限 r
 			for (int b = l; b <= r; b++) {
 				for (int c = l; c <= r; c++) {
 					if (Math.abs(b - c) <= K - 1) {
