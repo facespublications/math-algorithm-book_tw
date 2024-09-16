19ff46425c8a340100424f21d091d54e7115ba08
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:45:31 +0800

    Update Code_2_02_2.py

--- code/python/Code_2_02_2.py
+++ code/python/Code_2_02_2.py
@@ -1,4 +1,4 @@
-a, b = map(int, input().split()) # a と b を入力する
-print(a & b) # a AND b の値を出力する
-print(a | b) # a OR b の値を出力する
-print(a ^ b) # a XOR b の値を出力する
+a, b = map(int, input().split()) # 輸入 a 與 b
+print(a & b) # 輸出 a AND b 的値
+print(a | b) # 輸出 a OR b 的值
+print(a ^ b) # 輸出 a XOR b 的值
