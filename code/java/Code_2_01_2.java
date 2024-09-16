c4c9576b90962ce3afeda6592386199752ca4e25
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:11:24 +0800

    Update Code_2_01_2.java

--- code/java/Code_2_01_2.java
+++ code/java/Code_2_01_2.java
@@ -4,11 +4,11 @@ class Code_2_01_2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] A = new int[4];
-		// 入力部分
+		// 輸入部分
 		A[1] = sc.nextInt();
 		A[2] = sc.nextInt();
 		A[3] = sc.nextInt();
-		// 出力部分
+		// 輸出部分
 		System.out.println(A[1] + A[2] + A[3]);
 	}
 };
