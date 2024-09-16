4a724422c27710433274665dd994ce0f4ea0fa5a
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:29:13 +0800

    Update Code_5_09_1.java

--- code/java/Code_5_09_1.java
+++ code/java/Code_5_09_1.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Code_5_09_1 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		long N = sc.nextLong();
 		
-		// 支払い方のシミュレーション
+		// 支付方式的模擬
 		long answer = 0;
 		while (N >= 10000) {
-			N -= 10000; // 10000 円以上の間、10000 円札を使い続ける
+			N -= 10000; // 10000 圓以上則繼續使用 10000 圓紙鈔
 			answer += 1;
 		}
 		while (N >= 5000) {
-			N -= 5000; // 5000 円以上の間、5000 円札を使い続ける
+			N -= 5000; // 5000 圓以上則繼續使用 5000 圓紙鈔
 			answer += 1;
 		}
 		while (N >= 1) {
-			N -= 1000; // 残った金額を 1000 円札で支払う
+			N -= 1000; // 剩餘金額使用 1000 圓紙鈔支付
 			answer += 1;
 		}
 		
-		// 答えの出力
+		// 輸出答案
 		System.out.println(answer);
 	}
 }
