eea6321cf24b499d9084fb3dfa4859a4e3ce2212
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:21:29 +0800

    Update Code_4_05_3.py

--- code/python/Code_4_05_3.py
+++ code/python/Code_4_05_3.py
@@ -1,32 +1,32 @@
 import queue
 
-# 入力
+# 輸入
 N, M = map(int, input().split())
 A = [ None ] * M
 B = [ None ] * M
 for i in range(M):
 	A[i], B[i] = map(int, input().split())
 
-# 隣接リストの作成
+# 製作鄰接表
 G = [ list() for i in range(N + 1) ]
 for i in range(M):
 	G[A[i]].append(B[i])
 	G[B[i]].append(A[i])
 
-# 幅優先探索の初期化 (dist[i] = -1 のとき、未到達の白色頂点である）
+# 廣度優先搜尋的初始化（dist[i]=-1 時，為未到達的白色頂點）
 dist = [ -1 ] * (N + 1)
 Q = queue.Queue()
 dist[1] = 0
-Q.put(1) # Q に 1 を追加（操作 1）
+Q.put(1) # 將 1 添加到 Q 中（操作 1）
 
-# 幅優先探索
+# 廣度優先搜尋
 while not Q.empty():
-	pos = Q.get() # Q の先頭を調べ、これを取り出す（操作 2, 3）
+	pos = Q.get() # 查看 Q 的開頭並將之取出（操作2,3）
 	for nex in G[pos]:
 		if dist[nex] == -1:
 			dist[nex] = dist[pos] + 1
-			Q.put(nex) # Q に nex を追加（操作 1）
+			Q.put(nex) # 將 nex 添加到 Q 中（操作 1）
 
-# 頂点 1 から各頂点までの最短距離を出力
+# 輸出從頂點 1 到各頂點的最短距離
 for i in range(1, N + 1):
 	print(dist[i])
