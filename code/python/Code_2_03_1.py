9ad1c7c97da0898e4f95e878ee82f8f3f26296bb
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:46:45 +0800

    Update Code_2_03_1.py

--- code/python/Code_2_03_1.py
+++ code/python/Code_2_03_1.py
@@ -4,10 +4,10 @@ def func1():
 	return 2021
 
 def func2(pos):
-	global cnt # グローバル変数 cnt を関数の内部で使う
+	global cnt # 在函式內部使用全域變數 cnt
 	cnt += 1
 	return cnt + pos
 
-print(func1()) # 「2021」と出力
-print(func2(500)) # 「1501」と出力
-print(func2(500)) # 「1502」と出力
+print(func1()) # 輸出「2021」
+print(func2(500)) # 輸出「1501」
+print(func2(500)) # 輸出「1502」
