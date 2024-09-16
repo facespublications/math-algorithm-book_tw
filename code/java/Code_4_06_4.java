c3900b0d024d9f4040d61dfc01e9ebafaa81dff2
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:58:34 +0800

    Update Code_4_06_4.java

--- code/java/Code_4_06_4.java
+++ code/java/Code_4_06_4.java
@@ -10,7 +10,7 @@ class Code_4_06_4 {
 		System.out.println(answer);
 	}
 	static long modpow(long a, long b, long m) {
-		// 繰り返し二乗法（p は a^1, a^2, a^4, a^8, ... といった値をとる）
+		// 重複平方法（p 取 a^1、a^2、 a^4、a^8、⋯的值）
 		long p = a, answer = 1;
 		for (int i = 0; i < 30; i++) {
 			if ((b & (1 << i)) != 0) {
