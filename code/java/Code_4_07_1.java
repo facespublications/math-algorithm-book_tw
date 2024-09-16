3b903c7d2caffd79a5e836e9a78c8c7c3cd4c892
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:03:13 +0800

    Update Code_4_07_1.java

--- code/java/Code_4_07_1.java
+++ code/java/Code_4_07_1.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_4_07_1 {
 	public static void main(String[] args) {
-		// 入力 → 累乗の計算（N が 2 以上でなければ正しく動作しないので注意）
+		// 輸入　→　乘方的計算（請注意，N 必須大於 2 才能正常運作）
 		Scanner sc = new Scanner(System.in);
 		long N = sc.nextLong();
 		long[][] A = new long[2][2];
@@ -10,13 +10,13 @@ class Code_4_07_1 {
 		A[1][0] = 1; A[1][1] = 0;
 		long[][] B = copyMatrix(power(A, N - 1));
 		
-		// 答えの計算 → 出力（下から 9 桁目が 0 の場合、最初に 0 を含まない形で出力していることに注意）
+		// 輸出（請注意，倒數第 9 位為 0 時，以開頭不含 0 的形式輸出）
 		long answer = (B[1][0] + B[1][1]) % MOD;
 		System.out.println(answer);
 	}
 	static final int MOD = 1000000000;
 	static long[][] copyMatrix(long[][] A) {
-		// 行列のコピー（そのまま「M = A;」とコピーすると、参照がコピーされるだけなので注意）
+		// 複製矩陣（請留意若直接複製「M = A;」的話，只會複製參照）
 		long[][] M = new long[2][2];
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 2; j++) {
@@ -26,7 +26,7 @@ class Code_4_07_1 {
 		return M;
 	}
 	static long[][] multiplication(long[][] A, long[][] B) {
-		// 2×2 行列 A, B の積を返す関数
+		// 回傳　2×2 矩陣 A、B 乘積的函式
 		long[][] C = new long[2][2];
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 2; j++) {
@@ -39,7 +39,7 @@ class Code_4_07_1 {
 		return C;
 	}
 	static long[][] power(long[][] A, long n) {
-		// A の n 乗を返す関数
+		// 回傳 A 的 n 次方的函式
 		long[][] P = copyMatrix(A);
 		long[][] Q = { { 0, 0 }, { 0, 0 } };
 		boolean flag = false;
