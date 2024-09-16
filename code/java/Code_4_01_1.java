e31f33dc005b16494a798a1596b8b99e530e4488
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:32:48 +0800

    Update Code_4_01_1.java

--- code/java/Code_4_01_1.java
+++ code/java/Code_4_01_1.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class Code_4_01_1 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		long ax = sc.nextInt(), ay = sc.nextInt();
 		long bx = sc.nextInt(), by = sc.nextInt();
 		long cx = sc.nextInt(), cy = sc.nextInt();
 		
-		// ベクトル BA, BC, CA, CB の成分表示を求める
+		// 求向量BA、BC、CA、CB 的分量表示
 		long BAx = ax - bx, BAy = ay - by;
 		long BCx = cx - bx, BCy = cy - by;
 		long CAx = ax - cx, CAy = ay - cy;
 		long CBx = bx - cx, CBy = by - cy;
 		
-		// どのパターンに当てはまるか判定する
+		// 判斷適用哪一個模式
 		int pattern = 2;
 		if (BAx * BCx + BAy * BCy < 0L) {
 			pattern = 1;
@@ -23,7 +23,7 @@ class Code_4_01_1 {
 			pattern = 3;
 		}
 		
-		// 点と直線の距離を求める
+		// 求出點與直線的距離
 		double answer = 0.0;
 		if (pattern == 1) {
 			answer = Math.sqrt(BAx * BAx + BAy * BAy);
@@ -37,7 +37,7 @@ class Code_4_01_1 {
 			answer = S / BCLength;
 		}
 		
-		// 答えの出力
+		// 輸出答案
 		System.out.format("%.12f\n", answer);
 	}
 }
