f0a69d72b5fedf89fdaa54ef3429580b0d3baeaf
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:27:59 +0800

    Update Code_5_03_2.py

--- code/python/Code_5_03_2.py
+++ code/python/Code_5_03_2.py
@@ -1,9 +1,9 @@
-# 入力 → 数列の要素の総和 S を求める
+# 輸入 → 求數列元素的總和 S
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 S = sum(A)
 
-# 答えの出力
+# 輸出答案
 if S % 2 != K % 2:
 	print("No")
 elif S > K:
