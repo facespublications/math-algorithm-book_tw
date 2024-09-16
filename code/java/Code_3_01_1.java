83ebb8c0950820715ba33fc2ff65c02102625786
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:18:27 +0800

    Update Code_3_01_1.java

--- code/java/Code_3_01_1.java
+++ code/java/Code_3_01_1.java
@@ -13,10 +13,10 @@ class Code_3_01_1 {
 		}
 	}
 	static boolean isPrime(long N) {
-		// 2 以上の整数 N に対し、N が素数であれば true、素数でなければ false を返す関数
+		// 設 N 為 2 以上整數，若 N 為質數則返回 true，若不是質數則返回 false
 		for (long i = 2; i <= N - 1; i++) {
 			if (N % i == 0) {
-				return false; // N が i で割り切れた場合、この時点で素数ではないと分かる
+				return false; // N 被 i 整除時，此時可知不是質數
 			}
 		}
 		return true;
