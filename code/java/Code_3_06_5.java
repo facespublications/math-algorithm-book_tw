89da774c4c4eda7ac8f3b00809403df1778e5408
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:45:04 +0800

    Update Code_3_06_5.java

--- code/java/Code_3_06_5.java
+++ code/java/Code_3_06_5.java
@@ -4,7 +4,7 @@ class Code_3_06_5 {
 	static int N;
 	static int[] A;
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		A = new int[N + 1];
@@ -12,7 +12,7 @@ class Code_3_06_5 {
 			A[i] = sc.nextInt();
 		}
 		
-		// 再帰呼び出し → 答えの出力
+		// 遞迴呼叫→輸出答案
 		int answer = solve(1, N + 1);
 		System.out.println(answer);
 	}
@@ -20,9 +20,9 @@ class Code_3_06_5 {
 		if (r - l == 1) {
 			return A[l];
 		}
-		int m = (l + r) / 2; // 区間 [l, r) の中央で分割する
-		int s1 = solve(l, m); // s1 は A[l] + ... + A[m-1] の合計値となる
-		int s2 = solve(m, r); // s2 は A[m] + ... + A[r-1] の合計値となる
+		int m = (l + r) / 2; // 在區間 [l,r)的中央進行分割
+		int s1 = solve(l, m); // s1為 A[l]+...+A[m-1]的合計值
+		int s2 = solve(m, r); // s2為 A[m]+...+A[r-1] 的合計值
 		return s1 + s2;
 	}
 }
