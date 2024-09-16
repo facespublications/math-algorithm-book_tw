c2691ea8ea1ed344b28b343edb7a652ce43c8401
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:11:06 +0800

    Update Code_3_07_3.java

--- code/java/Code_3_07_3.java
+++ code/java/Code_3_07_3.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Code_3_07_3 {
 	public static void main(String[] args) {
-		// 入力
+		// 輸入
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int W = sc.nextInt();
@@ -13,7 +13,7 @@ class Code_3_07_3 {
 			v[i] = sc.nextInt();
 		}
 		
-		// 配列の初期化
+		// 陣列的初始化
 		long INF = 1000000000000000000L;
 		long[][] dp = new long[N + 1][W + 1];
 		dp[0][0] = 0;
@@ -21,21 +21,21 @@ class Code_3_07_3 {
 			dp[0][i] = -INF;
 		}
 		
-		// 動的計画法
+		// 動態規劃法
 		for (int i = 1; i <= N; i++) {
 			for (int j = 0; j <= W; j++) {
 				if (j < w[i]) {
-					// j < w[i] のとき、方法 A だけしか選べない
+					// j < w[i] 時，只能選擇方法 A
 					dp[i][j] = dp[i - 1][j];
 				}
 				else {
-					// j >= w[i] のとき、方法 A・方法 B どちらも選べる
+					// j >= w[i] 時，可以選擇方法 A、方法 B 的任一個
 					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
 				}
 			}
 		}
 		
-		// 答えを計算して出力
+		// 計算並輸出答案
 		long answer = 0;
 		for (int i = 0; i <= W; i++) {
 			answer = Math.max(answer, dp[N][i]);
