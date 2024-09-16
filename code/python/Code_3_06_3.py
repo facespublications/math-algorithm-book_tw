e6de393d68db38dd8aeb961c6d2dde1c460ce17d
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:01:04 +0800

    Update Code_3_06_3.py

--- code/python/Code_3_06_3.py
+++ code/python/Code_3_06_3.py
@@ -1,12 +1,12 @@
 def func(N):
 	if N == 1:
-		return 1  # このような場合分けすべきケースを「ベースケース」といいます
+		return 1  # 這種要區分開的情況稱為「基本情況」
 	return func(N - 1) * N
 
 N = int(input())
 print(func(N))
 
-# Python では、呼び出せる再帰関数の深さに上限が設定されており、デフォルトでは 1000 などの深さに設定されています。
-# この上限は、sys.getrecursionlimit() を呼び出すことで取得できます。
-# 一方、sys.setrecursionlimit(depth) を呼び出すことで、再帰呼び出しの深さ depth の上限を変えることができます。
-# （これらの機能を使うためには、最初に import sys と書く必要があります）
+# 在 Python 中，可呼叫的遞迴函數深度有一個上限，預設通常設為 1000 的深度。
+# 這個上限可以通過呼叫 sys.getrecursionlimit() 來取得。
+# 另外，可以通過呼叫 sys.setrecursionlimit(depth) 來更改遞迴呼叫深度的上限為指定的 depth。
+# （要使用這些功能，首先需要寫上 import sys）
