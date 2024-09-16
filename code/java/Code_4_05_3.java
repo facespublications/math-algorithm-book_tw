74af06dfedc837684b4a65890b104678085a2bd9
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 16:55:28 +0800

    Update Code_4_05_3.java

--- code/java/Code_4_05_3.java
+++ code/java/Code_4_05_3.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 class Code_4_05_3 {
 	public static void main(String[] args) throws IOException {
-		// 入力（高速な入出力のため、Scanner の代わりに BufferedReader を使っています）
+		// 輸入（由於是高速的輸入輸出，使用 BufferedReader 代替 Scanner）
 		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(buff.readLine());
@@ -17,7 +17,7 @@ class Code_4_05_3 {
 			B[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 隣接リストの作成
+		// 製作鄰接表
 		ArrayList<Integer>[] G = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
 			G[i] = new ArrayList<Integer>();
@@ -27,28 +27,28 @@ class Code_4_05_3 {
 			G[B[i]].add(A[i]);
 		}
 		
-		// 幅優先探索の初期化 (dist[i] = -1 のとき、未到達の白色頂点である）
+		// 廣度優先搜尋的初始化（dist[i]=-1 時，為未到達的白色頂點）
 		int[] dist = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
 			dist[i] = -1;
 		}
-		Queue<Integer> Q = new LinkedList<>(); // キュー Q を定義する
+		Queue<Integer> Q = new LinkedList<>(); // 定義佇列Q
 		dist[1] = 0;
-		Q.add(1); // Q に 1 を追加（操作 1）
+		Q.add(1); // 將 1 添加到 Q 中（操作 1）
 
-		// 幅優先探索
+		// 廣度優先搜尋
 		while (Q.size() >= 1) {
-			int pos = Q.remove(); // Q の先頭を調べ、これを取り出す（操作 2, 3）
+			int pos = Q.remove(); // 查看 Q 的開頭並將之取出（操作2,3）
 			for (int i = 0; i < G[pos].size(); i++) {
 				int nex = G[pos].get(i);
 				if (dist[nex] == -1) {
 					dist[nex] = dist[pos] + 1;
-					Q.add(nex); // Q に nex を追加（操作 1）
+					Q.add(nex); // 將　nex 添加到Q 中（操作 1）
 				}
 			}
 		}
 		
-		// 頂点 1 から各頂点までの最短距離を出力
+		// 輸出從頂點　1 到各頂點的最短距離
 		for (int i = 1; i <= N; i++) {
 			System.out.println(dist[i]);
 		}
