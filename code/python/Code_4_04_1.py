576587f79b0dc60678c6261799c14919145322d9
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:16:33 +0800

    Update Code_4_04_1.py

--- code/python/Code_4_04_1.py
+++ code/python/Code_4_04_1.py
@@ -1,16 +1,16 @@
-# 入力 → 配列 prime の初期化
+# 輸入 → 陣列 prime 的初始化
 N = int(input())
 prime = [ True ] * (N + 1)
 
-# エラトステネスのふるい
+# 埃拉托斯特尼篩
 LIMIT = int(N ** 0.5)
 for i in range(2, LIMIT + 1):
 	if prime[i] == True:
-		# x = 2i, 3i, 4i, ... と、N 以下の間ループし続ける
+		# x = 2i, 3i, 4i, ... 像這樣，N 以下的範圍不斷迴圈
 		for j in range(2 * i, N + 1, i):
 			prime[j] = False
 
-# N 以下の素数を小さい順に出力
+# 將 N 以下的質數以從小到大的順序輸出
 for i in range(2, N + 1):
 	if prime[i] == True:
 		print(i)
