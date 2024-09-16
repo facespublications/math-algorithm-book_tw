0365848130b1575f56bf9687b8694d2fb2c8516b
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:08:16 +0800

    Update Code_3_07_1.py

--- code/python/Code_3_07_1.py
+++ code/python/Code_3_07_1.py
@@ -1,17 +1,17 @@
-# 入力
+# 輸入
 N = int(input())
 H = list(map(int, input().split()))
 
-# 動的計画法
+# 動態規劃法
 dp = [ None ] * N
 dp[0] = 0
 for i in range(1, N):
 	if i == 1:
 		dp[i] = abs(H[i - 1] - H[i])
 	if i >= 2:
-		v1 = dp[i - 1] + abs(H[i - 1] - H[i])  # 1 個前の足場からジャンプするとき
-		v2 = dp[i - 2] + abs(H[i - 2] - H[i])  # 2 個前の足場からジャンプするとき
+		v1 = dp[i - 1] + abs(H[i - 1] - H[i])  # 跳躍到前 1 個跳台時
+		v2 = dp[i - 2] + abs(H[i - 2] - H[i])  # 跳躍到前 2 個跳台時
 		dp[i] = min(v1, v2)
 
-# 答えの出力
+# 輸出答案
 print(dp[N - 1])
