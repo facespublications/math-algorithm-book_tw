8c3505479c1728e2700ca35ff30457104a1c615c
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:40:45 +0800

    Update Code_2_01_4.py

--- code/python/Code_2_01_4.py
+++ code/python/Code_2_01_4.py
@@ -1,13 +1,13 @@
-N = int(input()) # 入力部分
+N = int(input()) # 輸入部分
 answer = ""
 
 while N >= 1:
-	# N % 2 は N を 2 で割った余り（例：N = 13 の場合 1）
-	# N // 2 は N を 2 で割った値の整数部分（例：N = 13 の場合 6）
+	# N % 2 為 N 除以 2 的餘數（例如：N=13 時為1）
+	# N // 2 為 N 除以 2 的值的整數部分（例如：N=13 時為6）
 	if N % 2 == 0:
 		answer = "0" + answer
 	if N % 2 == 1:
 		answer = "1" + answer
 	N = N // 2
 
-print(answer) # 出力部分
+print(answer) # 輸出部分
