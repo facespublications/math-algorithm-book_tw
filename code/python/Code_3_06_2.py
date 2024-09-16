cd5fb467d8c8ba4bfd8e3da7bb5ed62f2ba761f4
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 17:59:38 +0800

    Update Code_3_06_2.py

--- code/python/Code_3_06_2.py
+++ code/python/Code_3_06_2.py
@@ -1,18 +1,18 @@
-# 入力
+# 輸入
 N = int(input())
 A = list(map(int, input().split()))
 
-# 選択ソート
+# 選擇排序
 for i in range(N - 1):
 	min_position = i
 	min_value = A[i]
 	for j in range(i + 1, N):
 		if A[j] < min_value:
-			min_position = j  # min_position は最小値のインデックス（0 ～ N-1）
-			min_value = A[j]  # min_value は現時点での最小値
-	# A[i] と A[min_position] を交換
+			min_position = j  # min_position 為最小值的索引（0 ～ N-1）
+			min_value = A[j]  # min_value 為當下的最小值
+	# 交換 A[i] 與 A[min_position] 
 	A[i], A[min_position] = A[min_position], A[i]
 
-# 出力
+# 輸出
 for i in range(N):
 	print(A[i])
