dd3ce062dbe993c507d31e30614d5903182306e3
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 15:14:47 +0800

    Update Code_2_03_1.java

--- code/java/Code_2_03_1.java
+++ code/java/Code_2_03_1.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Code_2_03_1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(func1()); // 「2021」と出力
-		System.out.println(func2(500)); // 「1501」と出力
-		System.out.println(func2(500)); // 「1502」と出力
+		System.out.println(func1()); // 輸出「2021」
+		System.out.println(func2(500)); // 輸出「1501」
+		System.out.println(func2(500)); // 輸出「1502」
 	}
 	static int cnt = 1000;
 	static int func1() {
