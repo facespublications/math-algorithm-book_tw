ab0c3cadc1ad4637d257a10703c27d0688bad9b0
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:30:04 +0800

    Update Code_5_05_2.py

--- code/python/Code_5_05_2.py
+++ code/python/Code_5_05_2.py
@@ -1,35 +1,35 @@
 def check_numpoints(N, X, Y, lx, rx, ly, ry):
 	cnt = 0
 	for i in range(N):
-		# 点 (X[i], Y[i]) が長方形に含まれているかどうかを判定する
+		# 判定長方形中是否包含點 (X[i],Y[i])
 		if lx <= X[i] and X[i] <= rx and ly <= Y[i] and Y[i] <= ry:
 			cnt += 1
 	return cnt
 
-# 入力
+# 輸入
 N, K = map(int, input().split())
 X = [ None ] * N
 Y = [ None ] * N
 for i in range(N):
 	X[i], Y[i] = map(int, input().split())
 
-# 左端 x、右端 x、下端 y、上端 y を全探索（それぞれの番号が i, j, k, l）
-answer = 10 ** 19  # あり得ない値に設定
+# 將左端x、右端x、下端y 和上端y 進行全搜尋（各自的編號為i,j,k,l）
+answer = 10 ** 19  # 設定成不可能的值
 for i in range(N):
 	for j in range(N):
 		for k in range(N):
 			for l in range(N):
-				# cl <= x <= cr, dl <= y <= dr の長方形
-				# 長方形を作るためには、cl < cr, dl < dr である必要がある
+				# cl <= x <= cr, dl <= y <= dr 的長方形
+				# 要製作長方形，必須 cl < cr, dl < dr
 				cl, cr, dl, dr = X[i], X[j], Y[k], Y[l]
 				if cl < cr and dl < dr:
 					if check_numpoints(N, X, Y, cl, cr, dl, dr) >= K:
 						area = (cr - cl) * (dr - dl)
 						answer = min(answer, area)
 
-# 答えの出力
+# 輸出答案
 print(answer)
 
-# この問題は実行時間制限が厳しく、コード 5.5.2 の書き方をすると PyPy3 でも最大 2.5 秒程度の実行がかかり、AtCoder に提出すると TLE となります。
-# しかし、「cl < cr」と「dl < dr」を満たすものに限定して探索すると、探索する長方形の数がおよそ 1/4 になります。
-# これを利用したプログラムを PyPy3 で提出すると、1 秒以内に実行が終わり、正解 (AC) が得られます。
+# 這個問題對執行時間的限制很嚴格，如果使用程式碼 5.5.2 的寫法，即使在 PyPy3 上也可能需要最多 2.5 秒的執行時間，提交到 AtCoder 會導致 TLE（超過執行時間限制）。
+# 然而，如果限制只探索滿足「cl < cr」和「dl < dr」的情況，探索的長方形數量將大約減少至 1/4。
+# 利用這一點的程式如果在 PyPy3 上提交，則能在 1 秒內完成執行並得到正確答案（AC）。
