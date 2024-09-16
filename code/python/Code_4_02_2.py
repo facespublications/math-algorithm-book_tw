c67d55b8dac84acb88f3a9305cf19ea2d37acf58
facespublications <152489226+facespublications@users.noreply.github.com>
2024-09-16 18:15:07 +0800

    Update Code_4_02_2.py

--- code/python/Code_4_02_2.py
+++ code/python/Code_4_02_2.py
@@ -1,4 +1,4 @@
-# 入力
+# 輸入
 N, Q = map(int, input().split())
 L = [ None ] * Q
 R = [ None ] * Q
@@ -6,13 +6,13 @@ X = [ None ] * Q
 for i in range(Q):
 	L[i], R[i], X[i] = map(int, input().split())
 
-# 階差の計算
+# 階差的計算
 B = [ 0 ] * (N + 2)
 for i in range(Q):
 	B[L[i]] += X[i]
 	B[R[i] + 1] -= X[i]
 
-# 答えを計算して出力
+# 計算並輸出答案
 answer = ""
 for i in range(2, N + 1):
 	if B[i] > 0:
